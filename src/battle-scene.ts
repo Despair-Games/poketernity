e9fcaa0379672026fbb8c98a49b5f7326a44a6a5
PigeonBar <56974298+PigeonBar@users.noreply.github.com>
2024-12-11 20:35:55 -0500

    [P1] Minor bug fixes for PR #18 (#48)
    * [P1][Beta] Small bug fixes for PR #18
    
    * Replace `.findIndex()` with `.some()`

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -2912,7 +2912,7 @@ export default class BattleScene extends SceneBase {
         }
       }
     }
-    return true;
+    return success;
   }
 
   addEnemyModifier(modifier: PersistentModifier, ignoreUpdate?: boolean, instant?: boolean): void {
