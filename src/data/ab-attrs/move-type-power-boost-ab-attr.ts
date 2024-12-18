5444292346ddafb7ee469bf0f9dbfca4ae88b945
NightKev <34855794+DayKev@users.noreply.github.com>
2024-12-17 19:52:27 -0800

    Remove `??` from `MoveTypePowerBoostAbAttr`

--- src/data/ab-attrs/move-type-power-boost-ab-attr.ts
+++ src/data/ab-attrs/move-type-power-boost-ab-attr.ts
@@ -2,7 +2,7 @@ import type { Type } from "#enums/type";
 import { MovePowerBoostAbAttr } from "./move-power-boost-ab-attr";
 
 export class MoveTypePowerBoostAbAttr extends MovePowerBoostAbAttr {
-  constructor(boostedType: Type, powerMultiplier?: number) {
-    super((pokemon, _defender, move) => pokemon?.getMoveType(move) === boostedType, powerMultiplier ?? 1.5);
+  constructor(boostedType: Type, powerMultiplier: number = 1.5) {
+    super((pokemon, _defender, move) => pokemon?.getMoveType(move) === boostedType, powerMultiplier);
   }
 }
