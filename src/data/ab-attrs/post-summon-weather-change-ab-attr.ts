9372902c97d92153674cd21850795dcc91379e4c
NightKev <34855794+DayKev@users.noreply.github.com>
2024-12-17 21:42:17 -0800

    Remove pointless `if` statement from `PostSummonWeatherChangeAbAttr`
    Also add basic tsdocs to the class

--- src/data/ab-attrs/post-summon-weather-change-ab-attr.ts
+++ src/data/ab-attrs/post-summon-weather-change-ab-attr.ts
@@ -1,8 +1,13 @@
 import type Pokemon from "#app/field/pokemon";
 import { globalScene } from "#app/global-scene";
-import { WeatherType } from "#enums/weather-type";
+import type { WeatherType } from "#enums/weather-type";
 import { PostSummonAbAttr } from "./post-summon-ab-attr";
 
+/**
+ * Changes the weather if possible when a pokemon is summoned.
+ * @param weatherType The {@linkcode WeatherType} to set
+ * @extends PostSummonAbAttr
+ */
 export class PostSummonWeatherChangeAbAttr extends PostSummonAbAttr {
   private weatherType: WeatherType;
 
@@ -13,16 +18,10 @@ export class PostSummonWeatherChangeAbAttr extends PostSummonAbAttr {
   }
 
   override applyPostSummon(_pokemon: Pokemon, _passive: boolean, simulated: boolean, _args: any[]): boolean {
-    // TODO: This seems backwards, shouldn't it check that it's not one of these weathers?
-    const legendaryWeather = [WeatherType.HEAVY_RAIN, WeatherType.HARSH_SUN, WeatherType.STRONG_WINDS];
-    if (legendaryWeather.includes(this.weatherType) || !globalScene.arena.weather?.isImmutable()) {
-      if (simulated) {
-        return globalScene.arena.weather?.weatherType !== this.weatherType;
-      } else {
-        return globalScene.arena.trySetWeather(this.weatherType, true);
-      }
+    if (simulated) {
+      return globalScene.arena.weather?.weatherType !== this.weatherType;
+    } else {
+      return globalScene.arena.trySetWeather(this.weatherType, true);
     }
-
-    return false;
   }
 }
