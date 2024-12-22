e42af7434703b0a1855edc0a067325eaa5e80e55
frutescens <info@laptop>
2024-12-21 20:36:53 -0800

    Rewrote EffectSporeAbAttr. Will write docs later, cat in the way.

--- src/data/ab-attrs/effect-spore-ab-attr.ts
+++ src/data/ab-attrs/effect-spore-ab-attr.ts
@@ -4,25 +4,52 @@ import type { HitResult } from "#app/field/pokemon";
 import { Abilities } from "#enums/abilities";
 import { StatusEffect } from "#enums/status-effect";
 import { Type } from "#enums/type";
-import { PostDefendContactApplyStatusEffectAbAttr } from "./post-defend-contact-apply-status-effect-ab-attr";
+import { PostDefendAbAttr } from "./post-defend-ab-attr";
+import { MoveFlags } from "#app/data/move";
+import { randSeedInt } from "#app/utils";
 
-export class EffectSporeAbAttr extends PostDefendContactApplyStatusEffectAbAttr {
+export class EffectSporeAbAttr extends PostDefendAbAttr {
+  public readonly chance = 30;
+  private SLEEP_THRESHOLD = 11;
+  private PARA_THRESHOLD = 10 + this.SLEEP_THRESHOLD;
+  private PSN_THRESHOLD = 9 + this.PARA_THRESHOLD;
+
   constructor() {
-    super(10, StatusEffect.POISON, StatusEffect.PARALYSIS, StatusEffect.SLEEP);
+    super();
   }
 
   override applyPostDefend(
     pokemon: Pokemon,
-    passive: boolean,
+    _passive: boolean,
     simulated: boolean,
     attacker: Pokemon,
     move: Move,
-    hitResult: HitResult,
-    args: any[],
+    _hitResult: HitResult,
+    _args: any[],
   ): boolean {
     if (attacker.hasAbility(Abilities.OVERCOAT) || attacker.isOfType(Type.GRASS)) {
       return false;
     }
-    return super.applyPostDefend(pokemon, passive, simulated, attacker, move, hitResult, args);
+    const roll = randSeedInt(100);
+    if (move.checkFlag(MoveFlags.MAKES_CONTACT, attacker, pokemon) && !attacker.status && roll < this.chance) {
+      const statusEffect = this.getStatus(roll);
+      if (simulated) {
+        return attacker.canSetStatus(statusEffect, true, false, pokemon);
+      } else {
+        return attacker.trySetStatus(statusEffect, true, pokemon);
+      }
+    }
+    return false;
+  }
+
+  private getStatus(roll: number): StatusEffect {
+    if (roll < this.SLEEP_THRESHOLD) {
+      return StatusEffect.SLEEP;
+    } else if (roll < this.PARA_THRESHOLD) {
+      return StatusEffect.PARALYSIS;
+    } else if (roll < this.PSN_THRESHOLD) {
+      return StatusEffect.POISON;
+    }
+    return StatusEffect.NONE;
   }
 }
