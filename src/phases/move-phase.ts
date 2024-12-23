efb313ca91cdc31f4368eb0f75c3e609d8d09d10
innerthunder <brandonerickson98@gmail.com>
2024-12-23 01:46:40 -0800

    Fix import

--- src/phases/move-phase.ts
+++ src/phases/move-phase.ts
@@ -9,7 +9,7 @@ import { PostMoveUsedAbAttr } from "#app/data/ab-attrs/post-move-used-ab-attr";
 import type { DelayedAttackTag } from "#app/data/arena-tag";
 import { CommonAnim } from "#app/data/battle-anims";
 import { BattlerTagLapseType, CenterOfAttentionTag } from "#app/data/battler-tags";
-import { applyMoveAttrs, frenzyMissFunc } from "#app/data/move";
+import { applyMoveAttrs } from "#app/data/move";
 import { allMoves } from "#app/data/move";
 import { CopyMoveAttr } from "#app/data/move-attrs/copy-move-attr";
 import { BypassRedirectAttr } from "#app/data/move-attrs/bypass-redirect-attr";
@@ -42,6 +42,7 @@ import i18next from "i18next";
 import { getTerrainBlockMessage } from "#app/data/terrain";
 import { PokemonTypeChangeAbAttr } from "#app/data/ab-attrs/pokemon-type-change-ab-attr";
 import { DelayedAttackAttr } from "#app/data/move-attrs/delayed-attack-attr";
+import { frenzyMissFunc } from "#app/data/move-utils";
 
 export class MovePhase extends BattlePhase {
   protected _pokemon: Pokemon;
