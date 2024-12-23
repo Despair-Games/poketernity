d11cc73fee72fa8e1374290e5a586bf10b7e7866
innerthunder <brandonerickson98@gmail.com>
2024-12-22 17:49:57 -0800

    missed a not lol

--- src/phases/move-charge-phase.ts
+++ src/phases/move-charge-phase.ts
@@ -32,7 +32,7 @@ export class MoveChargePhase extends HitCheckPhase {
 
     const targetHitCheck = move.hitCheckOnCharge ? this.hitCheck(target)[0] : HitCheckResult.HIT;
 
-    if ([HitCheckResult.HIT, HitCheckResult.MISS].includes(targetHitCheck)) {
+    if (![HitCheckResult.HIT, HitCheckResult.MISS].includes(targetHitCheck)) {
       switch (targetHitCheck) {
         case HitCheckResult.NO_EFFECT:
           globalScene.queueMessage(
