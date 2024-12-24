70612d3b969c209e2a77c3d2c2ed2fe8cd0cebef
frutescens <info@laptop>
2024-12-23 16:38:41 -0800

    Wrote documentation and created tests. Not ready yet though.

--- src/data/ab-attrs/post-defend-contact-apply-status-effect-ab-attr.ts
+++ src/data/ab-attrs/post-defend-contact-apply-status-effect-ab-attr.ts
@@ -5,15 +5,25 @@ import type { HitResult } from "#app/field/pokemon";
 import type { StatusEffect } from "#enums/status-effect";
 import { PostDefendAbAttr } from "./post-defend-ab-attr";
 
+/**
+ * Ability attribute that inflicts a status on the attacking Pokemon if the attacker used a contact move on the ability holder
++--------------+-----------+----------+
+| Ability Name |  Status   | % Chance |
++--------------+-----------+----------+
+| Static       | Paralysis |       30 |
+| Flame Body   | Burn      |       30 |
+| Poison Point | Poison    |       30 |
++--------------+-----------+----------+ 
+ */
 export class PostDefendContactApplyStatusEffectAbAttr extends PostDefendAbAttr {
   public readonly chance: number;
   private readonly effects: StatusEffect[];
 
-  constructor(chance: number, ...effects: StatusEffect[]) {
+  constructor(chance: number, effects: StatusEffect | StatusEffect[]) {
     super();
 
     this.chance = chance;
-    this.effects = effects;
+    this.effects = this.effects.concat(effects);
   }
 
   override applyPostDefend(
@@ -30,12 +40,12 @@ export class PostDefendContactApplyStatusEffectAbAttr extends PostDefendAbAttr {
       && !attacker.status
       && (this.chance === -1 || pokemon.randSeedInt(100) < this.chance)
     ) {
-      const effect =
+      const status =
         this.effects.length === 1 ? this.effects[0] : this.effects[pokemon.randSeedInt(this.effects.length)];
       if (simulated) {
-        return attacker.canSetStatus(effect, true, false, pokemon);
+        return attacker.canSetStatus(status, true, false, pokemon);
       } else {
-        return attacker.trySetStatus(effect, true, pokemon);
+        return attacker.trySetStatus(status, true, pokemon);
       }
     }
 
