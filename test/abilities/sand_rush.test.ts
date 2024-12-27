c171898d29fc597b517c5ba042aed3f83ec00f43
frutescens <info@laptop>
2024-12-27 12:24:07 -0800

    Created tests for the unique properties of sand rush/slush rush outside of speed doubling.

---
+++ test/abilities/sand_rush.test.ts
@@ -0,0 +1,46 @@
+import { Abilities } from "#enums/abilities";
+import { Moves } from "#enums/moves";
+import { Species } from "#enums/species";
+import { WeatherType } from "#enums/weather-type";
+import { GameManager } from "#test/testUtils/gameManager";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+describe("Abilities - Sand Rush", () => {
+  // Note: The speed doubling properties of Sand Rush are tested in weather_based_speed_doubler.test.ts
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .weather(WeatherType.SANDSTORM)
+      .moveset([Moves.SPLASH])
+      .ability(Abilities.SAND_RUSH)
+      .battleType("single")
+      .disableCrits()
+      .enemySpecies(Species.MAGIKARP)
+      .enemyAbility(Abilities.BALL_FETCH)
+      .enemyMoveset(Moves.SPLASH);
+  });
+
+  it("should not take damage from sandstorm", async () => {
+    await game.classicMode.startBattle([Species.FEEBAS]);
+    const pokemon = game.scene.getPlayerPokemon();
+
+    game.move.select(Moves.SPLASH);
+    await game.phaseInterceptor.to("BerryPhase");
+
+    expect(pokemon?.isFullHp()).toBe(true);
+  });
+});
