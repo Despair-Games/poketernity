a2f8f52dcdf9d1c025dc5c6dd7bc8d56d026a511
Mumble <171087428+frutescens@users.noreply.github.com>
2025-01-05 17:51:37 -0800

    Update src/phases/stat-stage-change-phase.ts
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>

--- src/phases/stat-stage-change-phase.ts
+++ src/phases/stat-stage-change-phase.ts
@@ -121,7 +121,7 @@ export class StatStageChangePhase extends PokemonPhase {
             }),
           );
         } else {
-          messages.concat(this.getStatStageChangeMessages(filteredStats, stages.value, relLevels));
+          messages.push(...this.getStatStageChangeMessages(filteredStats, stages.value, relLevels));
         }
         for (const message of messages) {
           globalScene.queueMessage(message);
