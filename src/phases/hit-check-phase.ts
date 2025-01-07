c2d47d6591ae6f6e88f0a2e83ba33465cbc7dfc2
innerthunder <brandonerickson98@gmail.com>
2025-01-07 14:02:19 -0800

    Fix conflicts with `isProtected` logic

--- src/phases/hit-check-phase.ts
+++ src/phases/hit-check-phase.ts
@@ -97,8 +97,6 @@ export abstract class HitCheckPhase extends PokemonPhase {
     const targetSide = target.getArenaTagSide();
     /** Has the invoked move been cancelled by conditional protection (e.g Quick Guard)? */
     const hasConditionalProtectApplied = new BooleanHolder(false);
-    /** Does the applied conditional protection bypass Protect-ignoring effects? */
-    const bypassIgnoreProtect = new BooleanHolder(false);
     /** If the move is not targeting a Pokemon on the user's side, try to apply conditional protection effects */
     if (!this.move.getMove().isAllyTarget()) {
       globalScene.arena.applyTagsForSide(
@@ -109,15 +107,13 @@ export abstract class HitCheckPhase extends PokemonPhase {
         user,
         target,
         move.id,
-        bypassIgnoreProtect,
       );
     }
 
     /** Is the target protected by Protect, etc. or a relevant conditional protection effect? */
     const isProtected =
-      (bypassIgnoreProtect.value || !this.move.getMove().checkFlag(MoveFlags.IGNORE_PROTECT, user, target))
-      && (hasConditionalProtectApplied.value
-        || target.findTags((t) => t instanceof ProtectedTag)[0]?.apply(target, simulated, user, move));
+      hasConditionalProtectApplied.value
+      || target.findTags((t) => t instanceof ProtectedTag)[0]?.apply(target, simulated, user, move);
 
     if (isProtected) {
       return [HitCheckResult.PROTECTED, 0];
