6767667d5f33e5e3b6270c9466496fce1b0524ff
innerthunder <brandonerickson98@gmail.com>
2025-01-09 01:40:45 -0800

    `PostBattleInitAbAttr` cleanup

--- src/data/ab-attrs/post-battle-init-ab-attr.ts
+++ src/data/ab-attrs/post-battle-init-ab-attr.ts
@@ -2,6 +2,12 @@ import type { Pokemon } from "#app/field/pokemon";
 import { AbAttr } from "./ab-attr";
 
 export class PostBattleInitAbAttr extends AbAttr {
+  /**
+   * Applies an effect at the start of battle
+   * @param _pokemon The {@linkcode Pokemon} with this ability
+   * @param _simulated If `true`, suppresses changes to game state
+   * @returns `true` if the effect applies successfully
+   */
   override apply(_pokemon: Pokemon, _simulated: boolean): boolean {
     return false;
   }
--- src/data/ab-attrs/post-battle-init-stat-stage-change-ab-attr.ts
+++ src/data/ab-attrs/post-battle-init-stat-stage-change-ab-attr.ts
@@ -10,7 +10,7 @@ export class PostBattleInitStatStageChangeAbAttr extends PostBattleInitAbAttr {
   private readonly selfTarget: boolean;
 
   constructor(stats: BattleStat[], stages: number, selfTarget: boolean = false) {
-    super();
+    super(true, true);
 
     this.stats = stats;
     this.stages = stages;
@@ -35,7 +35,6 @@ export class PostBattleInitStatStageChangeAbAttr extends PostBattleInitAbAttr {
         if (!this.selfTarget && !statStageChangePhase.getPokemon()?.summonData) {
           globalScene.pushPhase(statStageChangePhase);
         } else {
-          // TODO: This causes the ability bar to be shown at the wrong time
           globalScene.unshiftPhase(statStageChangePhase);
         }
       }
