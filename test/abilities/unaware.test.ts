0b339061413e12d172a10fe7e2120a2916a1a5bb
Michael Li <michael5530@outlook.com>
2025-01-09 23:53:17 -0500

    Add more tests for Unaware edge cases

--- test/abilities/unaware.test.ts
+++ test/abilities/unaware.test.ts
@@ -1,10 +1,13 @@
+import { BattlerIndex } from "#app/battle";
+import { allMoves } from "#app/data/all-moves";
 import { Abilities } from "#enums/abilities";
+import { BattlerTagType } from "#enums/battler-tag-type";
 import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
 import { EFFECTIVE_STATS, Stat } from "#enums/stat";
 import { GameManager } from "#test/testUtils/gameManager";
 import Phaser from "phaser";
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 
 describe("Abilities - Unaware", () => {
   let phaserGame: Phaser.Game;
@@ -25,6 +28,7 @@ describe("Abilities - Unaware", () => {
     game.override
       .ability(Abilities.UNAWARE)
       .battleType("single")
+      .startingLevel(100)
       .disableCrits()
       .enemySpecies(Species.MAGIKARP)
       .enemyAbility(Abilities.BALL_FETCH);
@@ -64,11 +68,130 @@ describe("Abilities - Unaware", () => {
       if ([Stat.ATK, Stat.SPATK, Stat.SPD].includes(stat)) {
         expectedStat = expectedStat * 2;
       } else if ([Stat.DEF, Stat.SPDEF].includes(stat)) {
-        expectedStat = Math.floor((expectedStat * 2) / 3);
+        expectedStat = Math.floor(expectedStat * (2 / 3));
       }
 
       const actualStat = playerPokemon.getEffectiveStat(stat, enemyPokemon);
       expect(actualStat).toBe(expectedStat);
     }
+  });
+
+  it("should not cause the opponent's Stored Power to ignore the opponent's stat stages", async () => {
+    const storedPowerMove = allMoves[Moves.STORED_POWER];
+    vi.spyOn(storedPowerMove, "calculateBattlePower");
+
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.SHELL_SMASH);
+    await game.toNextTurn();
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.STORED_POWER);
+    await game.toNextTurn();
+
+    expect(storedPowerMove.calculateBattlePower).toHaveLastReturnedWith(140);
+  });
+
+  it("should not cause the move Punishment to ignore the opponent's stat stages", async () => {
+    const punishmentMove = allMoves[Moves.PUNISHMENT];
+    vi.spyOn(punishmentMove, "calculateBattlePower");
+
+    game.override.startingLevel(5).enemyLevel(100);
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.SHELL_SMASH);
+    await game.toNextTurn();
+    game.move.use(Moves.PUNISHMENT);
+    await game.move.forceEnemyMove(Moves.SPLASH);
+    await game.toNextTurn();
+
+    expect(punishmentMove.calculateBattlePower).toHaveLastReturnedWith(180);
+  });
+
+  it("should cause the opponent's Body Press to ignore the opponent's Defense stages", async () => {
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    const enemyPokemon = game.pokemonHelper.getEnemyPokemon();
+    vi.spyOn(enemyPokemon, "getEffectiveStat");
+    const expectedDef = enemyPokemon.getStat(Stat.DEF);
+
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.IRON_DEFENSE);
+    await game.toNextTurn();
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.BODY_PRESS);
+    await game.toNextTurn();
+
+    expect(enemyPokemon.getEffectiveStat).toHaveLastReturnedWith(expectedDef);
+  });
+
+  it("should not cause self-inflicted confusion to ignore stat stages", async () => {
+    game.override.statusActivation(true).startingLevel(1000).enemyLevel(1000);
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    const playerPokemon = game.pokemonHelper.getPlayerPokemon();
+    vi.spyOn(playerPokemon, "getEffectiveStat");
+    const enemyPokemon = game.pokemonHelper.getEnemyPokemon();
+    vi.spyOn(enemyPokemon, "getEffectiveStat");
+    const expectedPlayerAtk = Math.floor(playerPokemon.getStat(Stat.ATK) * (3 / 2));
+    const expectedEnemyAtk = Math.floor(enemyPokemon.getStat(Stat.ATK) * (3 / 2));
+    const expectedPlayerDef = Math.floor(playerPokemon.getStat(Stat.DEF) * (3 / 2));
+    const expectedEnemyDef = Math.floor(enemyPokemon.getStat(Stat.DEF) * (3 / 2));
+
+    game.move.use(Moves.VICTORY_DANCE);
+    await game.move.forceEnemyMove(Moves.VICTORY_DANCE);
+    await game.toNextTurn();
+
+    playerPokemon.addTag(BattlerTagType.CONFUSED);
+    enemyPokemon.addTag(BattlerTagType.CONFUSED);
+
+    game.move.use(Moves.SPLASH);
+    await game.move.forceEnemyMove(Moves.SPLASH);
+    await game.toNextTurn();
+
+    expect(playerPokemon.isFullHp()).toBe(false);
+    expect(enemyPokemon.isFullHp()).toBe(false);
+
+    // Check that each Pokemon's most recently computed stat is either their boosted Atk or their boosted Def
+    try {
+      expect(playerPokemon.getEffectiveStat).toHaveLastReturnedWith(expectedPlayerAtk);
+    } catch {
+      expect(playerPokemon.getEffectiveStat).toHaveLastReturnedWith(expectedPlayerDef);
+    }
+    try {
+      expect(enemyPokemon.getEffectiveStat).toHaveLastReturnedWith(expectedEnemyAtk);
+    } catch {
+      expect(enemyPokemon.getEffectiveStat).toHaveLastReturnedWith(expectedEnemyDef);
+    }
+  });
+
+  it("should not ignore an opponent's physical damage reduction from a Burn status", async () => {
+    // TODO: Is there a more direct way to test for Burn damage reduction?
+    vi.spyOn(allMoves[Moves.WILL_O_WISP], "accuracy", "get").mockReturnValue(-1);
+    game.override.startingLevel(1000).enemyLevel(1000);
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    const playerPokemon = game.pokemonHelper.getPlayerPokemon();
+    const hpAmounts = [playerPokemon.hp];
+
+    game.move.use(Moves.WILL_O_WISP);
+    await game.move.forceEnemyMove(Moves.TACKLE);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+    await game.toNextTurn();
+
+    hpAmounts.push(playerPokemon.hp);
+
+    game.move.use(Moves.WILL_O_WISP);
+    await game.move.forceEnemyMove(Moves.TACKLE);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+    await game.toNextTurn();
+
+    hpAmounts.push(playerPokemon.hp);
+
+    const damage1 = hpAmounts[0] - hpAmounts[1];
+    const damage2 = hpAmounts[1] - hpAmounts[2];
+
+    expect(damage2).toBeLessThan(damage1);
   });
 });
