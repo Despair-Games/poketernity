f034f1e310879f05fab4f4724ff0c69606cc7802
Temps Ray <temps.ray@gmail.com>
2025-01-12 17:30:05 -0500

    Address Moka's comments

--- test/abilities/early_bird.test.ts
+++ test/abilities/early_bird.test.ts
@@ -1,4 +1,3 @@
-import { Status } from "#app/data/status-effect";
 import { MoveResult } from "#enums/move-result";
 import { Abilities } from "#enums/abilities";
 import { Moves } from "#enums/moves";
