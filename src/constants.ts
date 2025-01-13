8364aeca4b3e817d8cabc1ffb91628846b2c23a4
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-01-13 12:40:39 -0800

    Reset `LS_PREFIX` to `pkty`
    Co-authored-by: Moka <54149968+MokaStitcher@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ export const SAVES_ZIP_PREFIX = "poketernity_";
 export const SAVE_FILE_EXTENSION = "txt";
 
 /** Prefix for local storage keys. */
-export const LS_PREFIX = "pky";
+export const LS_PREFIX = "pkty";
 
 /** Key for the local storage item storing the locale. */
 export const LOCALE_LS_KEY = `${LS_PREFIX}/locale`;
