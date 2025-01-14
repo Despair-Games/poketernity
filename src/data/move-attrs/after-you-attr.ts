580492ec334c09ad775226ca0b4231e7c471a39f
innerthunder <brandonerickson98@gmail.com>
2025-01-14 12:14:06 -0800

    Reimplement After You

--- src/data/move-attrs/after-you-attr.ts
+++ src/data/move-attrs/after-you-attr.ts
@@ -1,7 +1,6 @@
 import type { Pokemon } from "#app/field/pokemon";
 import { globalScene } from "#app/global-scene";
 import { getPokemonNameWithAffix } from "#app/messages";
-import { MovePhase } from "#app/phases/move-phase";
 import i18next from "i18next";
 import type { Move } from "#app/data/move";
 import { MoveEffectAttr } from "#app/data/move-attrs/move-effect-attr";
@@ -16,12 +15,8 @@ export class AfterYouAttr extends MoveEffectAttr {
   override apply(_user: Pokemon, target: Pokemon, _move: Move): boolean {
     globalScene.queueMessage(i18next.t("moveTriggers:afterYou", { targetName: getPokemonNameWithAffix(target) }));
 
-    //Will find next acting phase of the targeted pokémon, delete it and queue it next on successful delete.
-    const nextAttackPhase = globalScene.findPhase<MovePhase>((phase) => phase.pokemon === target);
-    if (nextAttackPhase && globalScene.tryRemovePhase((phase: MovePhase) => phase.pokemon === target)) {
-      globalScene.prependToPhase(new MovePhase(target, [...nextAttackPhase.targets], nextAttackPhase.move), MovePhase);
-    }
+    const { turnManager } = globalScene.currentBattle;
 
-    return true;
+    return turnManager.preemptFightCommand((tc) => tc.pokemon === target);
   }
 }
