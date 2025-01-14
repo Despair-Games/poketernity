08b5178bf158cdaaff50aebf51fc31756a07b5d2
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-01-14 12:43:01 -0800

    add abbrv. to save file, saves-zip & cookie name

--- src/constants.ts
+++ src/constants.ts
@@ -8,16 +8,16 @@ export const PLAYER_PARTY_MAX_SIZE: number = 6;
 export const USE_SEASONAL_SPLASH_MESSAGES: boolean = false;
 
 /** Name of the session ID cookie */
-export const SESSION_ID_COOKIE: string = "access_token";
+export const SESSION_ID_COOKIE: string = `${APP_ABBREVIATION}_access_token`;
 
 /** Max value for an integer attribute in {@linkcode SystemSaveData} */
 export const MAX_INT_ATTR_VALUE = 0x80000000;
 
 /** Prefix for zip file including all player saves. */
-export const SAVES_ZIP_PREFIX = "poketernity_";
+export const SAVES_ZIP_PREFIX = `${APP_ABBREVIATION}_`;
 
 /** File extension for save files. */
-export const SAVE_FILE_EXTENSION = "txt";
+export const SAVE_FILE_EXTENSION = `${APP_ABBREVIATION}.txt`;
 
 /** Prefix for local storage keys. */
 export const LS_PREFIX = APP_ABBREVIATION;
