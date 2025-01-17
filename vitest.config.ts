01b6380ad4dcbb4115bd487440a32c109ffacfe9
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-01-17 13:39:11 -0800

    chore: clean up vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,36 +2,13 @@ import { defineProject } from "vitest/config";
 import { defaultConfig } from "./vite.config";
 import { BaseSequencer, type TestSpecification } from "vitest/node";
 
-/**
- * A helper function for sorting test files in a desired order.
- *
- * A lower number means that a test file must be run earlier,
- * or else it breaks due to running tests with `--no-isolate.`
- */
-function getTestOrder(testName: string): number {
-  if (testName.includes("battle_scene.test.ts")) {
-    return 1;
-  } else if (testName.includes("inputs.test.ts")) {
-    return 2;
-  } else {
-    return 3;
-  }
-}
-
 export default defineProject(({ mode }) => ({
   ...defaultConfig,
   test: {
     testTimeout: 20000,
     setupFiles: ["./test/fontFace.setup.ts", "./test/vitest.setup.ts"],
     sequence: {
-      sequencer: class MySequencer extends BaseSequencer {
-        async sort(files: TestSpecification[]) {
-          files = await super.sort(files);
-
-          // Sort files so that certain tests get run first
-          return files.sort((a, b) => getTestOrder(a.moduleId) - getTestOrder(b.moduleId));
-        }
-      },
+      sequencer: MySequencer,
     },
     environment: "jsdom" as const,
     environmentOptions: {
@@ -56,3 +33,38 @@ export default defineProject(({ mode }) => ({
     keepNames: true,
   },
 }));
+
+//#region Helpers
+
+/**
+ * Class for sorting test files in the desired order.
+ */
+class MySequencer extends BaseSequencer {
+  async sort(files: TestSpecification[]) {
+    files = await super.sort(files);
+
+    return files.sort((a, b) => {
+      const aTestOrder = getTestOrder(a.moduleId);
+      const bTestOrder = getTestOrder(b.moduleId);
+      return aTestOrder - bTestOrder;
+    });
+  }
+}
+
+/**
+ * A helper function for sorting test files in a desired order.
+ *
+ * A lower number means that a test file must be run earlier,
+ * or else it breaks due to running tests with `--no-isolate.`
+ */
+function getTestOrder(testName: string): number {
+  if (testName.includes("battle_scene.test.ts")) {
+    return 1;
+  } else if (testName.includes("inputs.test.ts")) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+//#endregion
