65943ead3aacbffedb5bf225027f647ae9073bbc
frutescens <info@laptop>
2025-01-18 14:13:16 -0800

    Created Punk Rock defensive test

--- test/abilities/punk_rock.test.ts
+++ test/abilities/punk_rock.test.ts
@@ -3,7 +3,9 @@ import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
 import { GameManager } from "#test/testUtils/gameManager";
 import Phaser from "phaser";
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import { ReceivedMoveDamageMultiplierAbAttr } from "#app/data/ab-attrs/received-move-damage-multiplier-ab-attr";
+import type { NumberHolder } from "#app/utils";
 
 describe("Abilities - Punk Rock", () => {
   let phaserGame: Phaser.Game;
@@ -22,21 +24,25 @@ describe("Abilities - Punk Rock", () => {
   beforeEach(() => {
     game = new GameManager(phaserGame);
     game.override
-      .moveset([Moves.SPLASH])
+      .moveset([Moves.UPROAR])
       .ability(Abilities.BALL_FETCH)
       .battleType("single")
       .disableCrits()
       .enemySpecies(Species.MAGIKARP)
-      .enemyAbility(Abilities.BALL_FETCH)
+      .enemyAbility(Abilities.PUNK_ROCK)
       .enemyMoveset(Moves.SPLASH);
   });
 
-  it.todo("should receive 20% less damage from sound-based moves", async () => {
+  it("should receive 50% less damage from sound-based moves", async () => {
     await game.classicMode.startBattle([Species.FEEBAS]);
 
-    game.move.select(Moves.SPLASH);
+    const enemy = game.scene.getEnemyPokemon()!;
+    const abilitySpy = vi.spyOn(enemy.getAbility().getAttrs(ReceivedMoveDamageMultiplierAbAttr)[0], "apply");
+
+    game.move.select(Moves.UPROAR);
     await game.phaseInterceptor.to("BerryPhase");
 
-    expect(true).toBe(true);
+    const damageMultiplier = (abilitySpy.mock.lastCall?.[4] as NumberHolder).value;
+    expect(damageMultiplier).toBe(0.5);
   });
 });
