34a64963790347d0f3b2ba33054c8427ee477171
innerthunder <brandonerickson98@gmail.com>
2025-01-19 23:05:29 -0800

    Update + add disclaimer for Foul Play

--- src/data/move-attrs/target-atk-user-atk-attr.ts
+++ src/data/move-attrs/target-atk-user-atk-attr.ts
@@ -3,6 +3,7 @@ import type { Pokemon } from "#app/field/pokemon";
 import type { NumberHolder } from "#app/utils";
 import type { Move } from "#app/data/move";
 import { VariableAtkAttr } from "#app/data/move-attrs/variable-atk-attr";
+import { AbilityApplyMode } from "#enums/ability-apply-mode";
 
 /**
  * Attribute to set the offensive stat used for the move's attack to the target's Attack stat.
@@ -14,8 +15,12 @@ export class TargetAtkUserAtkAttr extends VariableAtkAttr {
     super();
   }
 
-  override apply(_user: Pokemon, target: Pokemon, _move: Move, attackingStat: NumberHolder): boolean {
-    attackingStat.value = target.getEffectiveStat(Stat.ATK, target);
+  /**
+   * @todo This wrongly ignores the user's Unaware and does not preserve the effects of Huge Power, etc.
+   * @see {@link https://github.com/Despair-Games/poketernity/issues/184 | #184}
+   */
+  override apply(_user: Pokemon, target: Pokemon, move: Move, attackingStat: NumberHolder): boolean {
+    attackingStat.value = target.getEffectiveStat(Stat.ATK, target, move, AbilityApplyMode.IGNORE);
     return true;
   }
 }
