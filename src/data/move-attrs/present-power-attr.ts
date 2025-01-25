3665fd0c2f0dd04ffee4344382111a9c64d74ef9
frutescens <info@laptop>
2025-01-25 13:31:51 -0800

    Actual Reset

--- src/data/move-attrs/present-power-attr.ts
+++ src/data/move-attrs/present-power-attr.ts
@@ -1,9 +1,5 @@
 import type { Pokemon } from "#app/field/pokemon";
-import { globalScene } from "#app/global-scene";
-import { getPokemonNameWithAffix } from "#app/messages";
-import { PokemonHealPhase } from "#app/phases/pokemon-heal-phase";
-import { randSeedInt, type NumberHolder, toDmgValue } from "#app/utils";
-import i18next from "i18next";
+import { type NumberHolder } from "#app/utils";
 import type { Move } from "#app/data/move";
 import { VariablePowerAttr } from "#app/data/move-attrs/variable-power-attr";
 
@@ -14,31 +10,21 @@ import { VariablePowerAttr } from "#app/data/move-attrs/variable-power-attr";
  * @extends VariablePowerAttr
  */
 export class PresentPowerAttr extends VariablePowerAttr {
-  override apply(user: Pokemon, target: Pokemon, _move: Move, power: NumberHolder): boolean {
+  override apply(_user: Pokemon, _target: Pokemon, move: Move, power: NumberHolder): boolean {
     /**
      * If this move is multi-hit, and this attribute is applied to any hit
      * other than the first, this move cannot result in a heal.
      */
-    const firstHit = user.turnData.hitCount === user.turnData.hitsLeft;
 
-    const powerSeed = randSeedInt(firstHit ? 100 : 80);
-    if (powerSeed <= 40) {
+    const powerSeed = move.chance;
+    if (powerSeed < 40) {
       power.value = 40;
-    } else if (40 < powerSeed && powerSeed <= 70) {
+    } else if (powerSeed < 70) {
       power.value = 80;
-    } else if (70 < powerSeed && powerSeed <= 80) {
+    } else if (powerSeed < 80) {
       power.value = 120;
-    } else if (80 < powerSeed && powerSeed <= 100) {
-      // If this move is multi-hit, disable all other hits
-      user.turnData.hitCount = 1;
-      user.turnData.hitsLeft = 1;
-      globalScene.unshiftPhase(
-        new PokemonHealPhase(target.getBattlerIndex(), toDmgValue(target.getMaxHp() / 4), {
-          message: i18next.t("moveTriggers:regainedHealth", { pokemonName: getPokemonNameWithAffix(target) }),
-        }),
-      );
     }
-
+    move.chance = 0;
     return true;
   }
 }
