e0c6416d69d7e960fa9abb63ddc9e3768cf4a220
frutescens <info@laptop>
2025-01-25 13:30:32 -0800

    Resetting

--- src/data/move-attrs/heal-on-ally-attr.ts
+++ src/data/move-attrs/heal-on-ally-attr.ts
@@ -1,15 +1,22 @@
 import type { Pokemon } from "#app/field/pokemon";
 import type { Move } from "#app/data/move";
 import { HealAttr } from "#app/data/move-attrs/heal-attr";
+import type { MoveConditionFunc } from "../move-conditions";
 
 /**
  * Heals the target only if it is the ally.
  * Used for {@link https://bulbapedia.bulbagarden.net/wiki/Pollen_Puff_(move) | Pollen Puff}.
  * @extends HealAttr
  */
-export class HealOnAllyAttr extends HealAttr {
+export class ConditionalHealAttr extends HealAttr {
+  private healCondition: MoveConditionFunc;
+
+  constructor(healRatio: number, selfTarget: boolean, healCondition: MoveConditionFunc) {
+    super(healRatio, true, selfTarget);
+    this.healCondition = healCondition;
+  }
   override apply(user: Pokemon, target: Pokemon, move: Move): boolean {
-    if (user.getAlly() === target) {
+    if (this.healCondition(user, target, move)) {
       super.apply(user, target, move);
       return true;
     }
