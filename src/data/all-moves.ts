49ac9fbe685f412e8926c15a91075800b5c602da
NightKev <34855794+DayKev@users.noreply.github.com>
2025-01-26 22:02:03 -0800

    Note missing uncopiable move list for Mirror Move
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>

--- src/data/all-moves.ts
+++ src/data/all-moves.ts
@@ -572,7 +572,9 @@ export function initMoves() {
       .target(MoveTarget.USER)
       .unimplemented(),
     new SelfStatusMove(Moves.METRONOME, Type.NORMAL, -1, 10, -1, 0, 1).attr(RandomMoveAttr, invalidMetronomeMoves),
-    new StatusMove(Moves.MIRROR_MOVE, Type.FLYING, -1, 20, -1, 0, 1).attr(CopyMoveAttr, true),
+    new StatusMove(Moves.MIRROR_MOVE, Type.FLYING, -1, 20, -1, 0, 1)
+      .attr(CopyMoveAttr, true)
+      .edgeCase(), // Need to find out which moves are not copyable by Mirror Move
     new AttackMove(Moves.SELF_DESTRUCT, Type.NORMAL, MoveCategory.PHYSICAL, 200, 100, 5, -1, 0, 1)
       .attr(SacrificialAttr)
       .makesContact(false)
