27cdad54703f3469ffd7bad3fe52eb44733f3c8b
frutescens <info@laptop>
2025-01-26 22:50:33 -0800

    Added colors enum

---
+++ src/enums/pokemon-colors.ts
@@ -0,0 +1,17 @@
+/**
+ * In Pokemon HOME, this information refers to a Pokemon's main color.
+ * Per Bulbapedia, "While the actual colors of individual Pokémon can vary, the color associated with a species is usually the color most apparent or covering the Pokémon's body."
+ */
+
+export enum PokemonColors {
+  BLACK = 1,
+  BLUE,
+  BROWN,
+  GRAY,
+  GREEN,
+  PINK,
+  PURPLE,
+  RED,
+  WHITE,
+  YELLOW,
+}
