dd9585e29fc9f3c93e5dad6a447d3bf1b9d663b4
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-01-29 01:22:31 -0800

    Rewrite `forceEnemyToSwitch` util + more test fixes

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -5674,7 +5674,7 @@ export class EnemyPokemon extends Pokemon {
     if (
       trainer
       && !this.isTrapped()
-      && this.getParty().some((p) => !p.isActive(true))
+      && this.getParty().some((p) => p.isActive() && !p.isOnField())
       && this.getMoveQueue().length === 0
     ) {
       const switchCommand = this.getSwitchCommand();
@@ -5699,7 +5699,7 @@ export class EnemyPokemon extends Pokemon {
    * @todo Finalize the MUS threshold for switching
    */
   public getSwitchCommand(): TurnCommand | undefined {
-    const nonActiveParty = this.getParty().filter((p) => !p.isActive(true));
+    const nonActiveParty = this.getParty().filter((p) => p.isActive() && !p.isOnField());
     const matchupScore = this.getAverageMatchupScore();
 
     // If this Pokemon can KO at least 1 opponent, it gains an average MUS
--- test/moves/focus_punch.test.ts
+++ test/moves/focus_punch.test.ts
@@ -109,8 +109,8 @@ describe("Moves - Focus Punch", () => {
 
     await game.startBattle([Species.CHARIZARD]);
 
-    game.forceEnemyToSwitch();
     game.move.select(Moves.FOCUS_PUNCH);
+    await game.forceEnemyToSwitch();
 
     await game.phaseInterceptor.to(TurnStartPhase);
 
