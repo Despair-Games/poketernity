9d85785f1222604794ebe4f872c816d8042f237f
frutescens <info@laptop>
2025-02-02 19:35:02 -0800

    More small fixes

--- src/data/move-attrs/target-half-hp-damage-attr.ts
+++ src/data/move-attrs/target-half-hp-damage-attr.ts
@@ -14,38 +14,14 @@ import { FixedDamageAttr } from "#app/data/move-attrs/fixed-damage-attr";
 export class TargetHalfHpDamageAttr extends FixedDamageAttr {
   // the initial amount of hp the target had before the first hit
   // used for multi lens
-  private initialHp: number;
   constructor() {
     super(0);
   }
 
-  override apply(user: Pokemon, target: Pokemon, _move: Move, damage: NumberHolder): boolean {
-    // first, determine if the hit is coming from multi lens or not
-    const lensCount =
-      user
-        .getHeldItems()
-        .find((i) => i.isPokemonMultiHitModifier())
-        ?.getStackCount() ?? 0;
-    if (lensCount <= 0) {
-      // no multi lenses; we can just halve the target's hp and call it a day
-      damage.value = toDmgValue(target.hp / 2);
-      return true;
-    }
-
-    // figure out what hit # we're on
-    switch (user.turnData.hitCount - user.turnData.hitsLeft) {
-      case 0:
-        // first hit of move; update initialHp tracker
-        this.initialHp = target.hp;
-      default:
-        // multi lens added hit; use initialHp tracker to ensure correct damage
-        damage.value = toDmgValue(this.initialHp / 2);
-        return true;
-      case lensCount + 1:
-        // parental bond added hit; calc damage as normal
-        damage.value = toDmgValue(target.hp / 2);
-        return true;
-    }
+  override apply(_user: Pokemon, target: Pokemon, _move: Move, damage: NumberHolder): boolean {
+    // no multi lenses; we can just halve the target's hp and call it a day
+    damage.value = toDmgValue(target.hp / 2);
+    return true;
   }
 
   override getTargetBenefitScore(_user: Pokemon, target: Pokemon, _move: Move): number {
--- src/data/move.ts
+++ src/data/move.ts
@@ -3,7 +3,7 @@ import { type Pokemon } from "#app/field/pokemon";
 import { globalScene } from "#app/global-scene";
 import type { Localizable } from "#app/interfaces/locales";
 import { getPokemonNameWithAffix } from "#app/messages";
-import { PokemonMoveAccuracyBoosterModifier, AttackTypeBoosterModifier } from "#app/modifier/modifier";
+import { AttackTypeBoosterModifier } from "#app/modifier/modifier";
 import type { AbstractConstructor, Constructor, nil } from "#app/utils";
 import { BooleanHolder, NumberHolder } from "#app/utils";
 import { Abilities } from "#enums/abilities";
@@ -768,7 +768,6 @@ export abstract class Move implements Localizable {
       && power.value < 60
       && this.priority <= 0
       && !this.hasAttr(MultiHitAttr)
-      && !globalScene.findModifier((m) => m.isPokemonMultiHitModifier() && m.pokemonId === source.id)
     ) {
       power.value = 60;
     }
