7b9b5ccbf7b7fb6d20a9e831e488606a96c7b270
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-02-03 12:11:26 -0800

    eliminate `allMoves` from protect-attr & resist-last-move-type-attr
    16 dependency violations

--- src/data/move-attrs/resist-last-move-type-attr.ts
+++ src/data/move-attrs/resist-last-move-type-attr.ts
@@ -8,7 +8,6 @@ import i18next from "i18next";
 import { applyChallenges } from "#app/utils/challenge-utils";
 import { ChallengeType } from "#enums/challenge-type";
 import { type Move } from "#app/data/move";
-import { allMoves } from "#app/data/all-moves";
 import { getTypeDamageMultiplier } from "#app/data/type";
 import { MoveEffectAttr } from "#app/data/move-attrs/move-effect-attr";
 import type { MoveConditionFunc } from "#app/@types/MoveConditionFunc";
@@ -33,8 +32,8 @@ export class ResistLastMoveTypeAttr extends MoveEffectAttr {
       return false;
     }
 
-    const moveData = allMoves[targetMove.moveId];
-    if (moveData.type === ElementalType.STELLAR || moveData.type === ElementalType.UNKNOWN) {
+    const moveData = targetMove.move;
+    if (!moveData || [ElementalType.STELLAR, ElementalType.UNKNOWN].includes(moveData?.type)) {
       return false;
     }
     const userTypes = user.getTypes();
