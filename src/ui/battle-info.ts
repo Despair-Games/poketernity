b02706e7e8a48e387ba7517384d29d79ea3731ee
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-02-02 21:41:22 -0800

    chore: fix eslint

--- src/ui/battle-info.ts
+++ src/ui/battle-info.ts
@@ -17,7 +17,7 @@ import { WindowVariant } from "#enums/window-variant";
 import i18next from "i18next";
 import { ExpGainsSpeed } from "#enums/exp-gains-speed";
 import { settings } from "#app/system/settings/settings-manager";
-import { CANVAS_SCALE, GAME_WIDTH } from "#app/ui-constants";
+import { CANVAS_SCALE } from "#app/ui-constants";
 
 export default class BattleInfo extends Phaser.GameObjects.Container {
   public static readonly EXP_GAINS_DURATION_BASE = 1650;
