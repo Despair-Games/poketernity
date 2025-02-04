fcece35bc8b1ba17e3d09092469fd1a454083420
frutescens <info@laptop>
2025-02-04 11:22:38 -0800

    Replaced spyOn check with abilitiesApplied check

--- test/abilities/anticipation.test.ts
+++ test/abilities/anticipation.test.ts
@@ -1,12 +1,10 @@
-import { AnticipationAbAttr } from "#app/data/ab-attrs/anticipation-ab-attr";
-import { allAbilities } from "#app/data/ability";
 import { Abilities } from "#enums/abilities";
 import { ElementalType } from "#enums/elemental-type";
 import { MoveId } from "#enums/move-id";
 import { Species } from "#enums/species";
 import { GameManager } from "#test/testUtils/gameManager";
 import Phaser from "phaser";
-import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 
 describe("Abilities - Anticipation", () => {
   let phaserGame: Phaser.Game;
@@ -34,51 +32,47 @@ describe("Abilities - Anticipation", () => {
 
   it("should activate when the opponent has a super-effective move", async () => {
     game.override.enemyMoveset(MoveId.ABSORB);
-    const anticipationAbility = allAbilities[Abilities.ANTICIPATION].getAttrs(AnticipationAbAttr)[0];
-    vi.spyOn(anticipationAbility, "apply");
     await game.classicMode.startBattle([Species.FEEBAS]);
+    const playerPokemon = game.scene.getPlayerPokemon()!;
 
-    expect(anticipationAbility.apply).toHaveLastReturnedWith(true);
+    expect(playerPokemon.battleData.abilitiesApplied[0]).toBe(Abilities.ANTICIPATION);
   });
 
   it("should activate when the opponent has a 1HKO move", async () => {
     game.override.enemyMoveset(MoveId.FISSURE);
-    const anticipationAbility = allAbilities[Abilities.ANTICIPATION].getAttrs(AnticipationAbAttr)[0];
-    vi.spyOn(anticipationAbility, "apply");
     await game.classicMode.startBattle([Species.FEEBAS]);
+    const playerPokemon = game.scene.getPlayerPokemon()!;
 
-    expect(anticipationAbility.apply).toHaveLastReturnedWith(true);
+    expect(playerPokemon.battleData.abilitiesApplied[0]).toBe(Abilities.ANTICIPATION);
   });
 
   it("should not activate when the opponent does not have a super-effective or 1HKO move", async () => {
     game.override.enemyMoveset(MoveId.SPLASH);
-    const anticipationAbility = allAbilities[Abilities.ANTICIPATION].getAttrs(AnticipationAbAttr)[0];
-    vi.spyOn(anticipationAbility, "apply");
     await game.classicMode.startBattle([Species.FEEBAS]);
+    const playerPokemon = game.scene.getPlayerPokemon()!;
 
-    expect(anticipationAbility.apply).toHaveLastReturnedWith(false);
+    expect(playerPokemon.battleData.abilitiesApplied.length).toBe(0);
   });
 
   it("should consider Hidden Power's calculated type, not its default Normal type", async () => {
     game.override.enemyMoveset(MoveId.HIDDEN_POWER).enemyIVs([31, 31, 31, 30, 31, 31]);
     // Hidden Power type set to Electric here
-    const anticipationAbility = allAbilities[Abilities.ANTICIPATION].getAttrs(AnticipationAbAttr)[0];
-    vi.spyOn(anticipationAbility, "apply");
     await game.classicMode.startBattle([Species.FEEBAS]);
-    const enemyPokemon = game.scene.getEnemyPokemon();
-    expect(enemyPokemon?.getMoveType(enemyPokemon.getMoveset()[0].getMove())).toBe(ElementalType.ELECTRIC);
-    expect(anticipationAbility.apply).toHaveLastReturnedWith(true);
+    const enemyPokemon = game.scene.getEnemyPokemon()!;
+    const playerPokemon = game.scene.getPlayerPokemon()!;
+
+    expect(enemyPokemon.getMoveType(enemyPokemon.getMoveset()[0].getMove())).toBe(ElementalType.ELECTRIC);
+    expect(playerPokemon.battleData.abilitiesApplied[0]).toBe(Abilities.ANTICIPATION);
   });
 
   it("should not consider most variable-type moves' calculated type", async () => {
     game.override.enemySpecies(Species.PIKACHU).enemyMoveset(MoveId.REVELATION_DANCE);
 
-    const anticipationAbility = allAbilities[Abilities.ANTICIPATION].getAttrs(AnticipationAbAttr)[0];
-    vi.spyOn(anticipationAbility, "apply");
     await game.classicMode.startBattle([Species.FEEBAS]);
-    const enemyPokemon = game.scene.getEnemyPokemon();
+    const enemyPokemon = game.scene.getEnemyPokemon()!;
+    const playerPokemon = game.scene.getPlayerPokemon()!;
 
-    expect(enemyPokemon?.getMoveType(enemyPokemon.getMoveset()[0].getMove())).toBe(ElementalType.ELECTRIC);
-    expect(anticipationAbility.apply).toHaveLastReturnedWith(false);
+    expect(enemyPokemon.getMoveType(enemyPokemon.getMoveset()[0].getMove())).toBe(ElementalType.ELECTRIC);
+    expect(playerPokemon.battleData.abilitiesApplied.length).toBe(0);
   });
 });
