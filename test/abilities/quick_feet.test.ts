7eee809ffab266f33a4c552a98f3137dd1b04464
Mumble <171087428+frutescens@users.noreply.github.com>
2025-02-08 15:18:19 -0800

    Update test/abilities/quick_feet.test.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- test/abilities/quick_feet.test.ts
+++ test/abilities/quick_feet.test.ts
@@ -36,7 +36,7 @@ describe("Abilities - Quick Feet", () => {
   it("should increase the Pokemon's speed by 50% if it has a status effect", async () => {
     game.override.statusEffect(StatusEffect.POISON);
     await game.classicMode.startBattle([Species.FEEBAS]);
-    const playerPokemon = game.scene.getPlayerPokemon()!;
+    const playerPokemon = game.field.getPlayerPokemon();
     const speedStat = playerPokemon.getStat(Stat.SPD);
     const effectiveSpeedStat = playerPokemon.getEffectiveStat(Stat.SPD);
 
