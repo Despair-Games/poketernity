5730ba1f49a7805ba7162eddd632563a73ec5acd
frutescens <info@laptop>
2025-02-08 14:54:31 -0800

    Wrote test and added new attribute to ignore paralysis speed reduction

---
+++ src/data/ab-attrs/bypass-para-speed-reduction-ab-attr.ts
@@ -0,0 +1,18 @@
+import type { Pokemon } from "#app/field/pokemon";
+import type { BooleanHolder } from "#app/utils";
+import { AbAttr } from "./ab-attr";
+
+/**
+ * Ability attribute that allows the ability holder to ignore the speed reduction from Paralysis.
+ * Used by the ability Quick Feet
+ */
+export class BypassParaSpeedReductionAbAttr extends AbAttr {
+  constructor() {
+    super(false);
+  }
+
+  override apply(_pokemon: Pokemon, _simulated: boolean, cancelled: BooleanHolder): boolean {
+    cancelled.value = true;
+    return true;
+  }
+}
