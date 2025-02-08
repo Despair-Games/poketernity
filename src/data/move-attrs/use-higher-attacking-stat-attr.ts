d1c8623e972b1bebecb26047dd07b21459e28276
Temps Ray <163687446+Tempo-anon@users.noreply.github.com>
2025-02-08 17:08:16 -0500

    UseHigherAttackingStatAttr only returns true if the move category changes

--- src/data/move-attrs/use-higher-attacking-stat-attr.ts
+++ src/data/move-attrs/use-higher-attacking-stat-attr.ts
@@ -14,14 +14,21 @@ import { AbilityApplyMode } from "#enums/ability-apply-mode";
  */
 export class UseHigherAttackingStatAttr extends VariableMoveCategoryAttr {
   override apply(user: Pokemon, target: Pokemon, move: Move, category: NumberHolder): boolean {
+    let returnVal = false;
     if (
       user.getEffectiveStat(Stat.ATK, target, move, AbilityApplyMode.IGNORE)
       > user.getEffectiveStat(Stat.SPATK, target, move, AbilityApplyMode.IGNORE)
     ) {
+      if (category.value === MoveCategory.SPECIAL) {
+        returnVal = true;
+      }
       category.value = MoveCategory.PHYSICAL;
     } else {
+      if (category.value === MoveCategory.PHYSICAL) {
+        returnVal = true;
+      }
       category.value = MoveCategory.SPECIAL;
     }
-    return true;
+    return returnVal;
   }
 }
