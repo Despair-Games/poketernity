36038462302cb7929a8de707534e62b994095c36
PigeonBar <56974298+PigeonBar@users.noreply.github.com>
2025-02-08 19:32:13 -0500

    Moved function to init file, still gets executed in setup file

--- test/testUtils/testFileInitialization.ts
+++ test/testUtils/testFileInitialization.ts
@@ -1,4 +1,15 @@
+import { initLoggedInUser } from "#app/account";
 import { SESSION_ID_COOKIE } from "#app/constants";
+import { initAbilities } from "#app/data/all-abilities";
+import { allMoves, initMoves } from "#app/data/all-moves";
+import { initBiomes } from "#app/data/balance/biomes";
+import { initEggMoves } from "#app/data/balance/egg-moves";
+import { initPokemonPrevolutions } from "#app/data/balance/pokemon-evolutions";
+import { initMysteryEncounters } from "#app/data/mystery-encounters/mystery-encounters";
+import { initPokemonForms } from "#app/data/pokemon-forms";
+import { initSpecies } from "#app/data/init-species";
+import { initAchievements } from "#app/system/achv";
+import { initStatsKeys } from "#app/ui/game-stats-ui-handler";
 import { setCookie } from "#app/utils";
 import { blobToString } from "#test/testUtils/gameManagerUtils";
 import { MockConsoleLog } from "#test/testUtils/mocks/mockConsoleLog";
@@ -7,6 +18,28 @@ import { mockLocalStorage } from "#test/testUtils/mocks/mockLocalStorage";
 import { MockImage } from "#test/testUtils/mocks/mocksContainer/mockImage";
 import Phaser from "phaser";
 import { manageListeners } from "./listenersManager";
+import { initVouchers } from "#app/system/init-vouchers";
+
+/**
+ * A function to initialize game data before running any other test-related code.
+ */
+export function initDataForTests() {
+  // Initialize all of these things if and only if they have not been initialized yet
+  if (Object.values(allMoves).length === 0) {
+    initMoves();
+    initVouchers();
+    initAchievements();
+    initStatsKeys();
+    initPokemonPrevolutions();
+    initBiomes();
+    initEggMoves();
+    initPokemonForms();
+    initSpecies();
+    initAbilities();
+    initLoggedInUser();
+    initMysteryEncounters();
+  }
+}
 
 /**
  * An initialization function that is run at the beginning of every test file (via `beforeAll()`).
