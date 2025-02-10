2b2559c06f0d5ab6f34069ec3a74c68067a327d5
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-02-09 19:38:37 -0800

    ...... use `i18next.t` instead of simply, short an easy `t`

--- src/utils/item-utils.ts
+++ src/utils/item-utils.ts
@@ -2,14 +2,14 @@ import type { PlayerPokemon } from "#app/field/pokemon";
 import { globalScene } from "#app/global-scene";
 import { getPokemonNameWithAffix } from "#app/messages";
 import type { PokemonHeldItemModifier } from "#app/modifier/modifier";
-import { t } from "i18next";
+import i18next from "i18next";
 
 export const FilterItemMaxStacks = (pokemon: PlayerPokemon, modifier: PokemonHeldItemModifier) => {
   const matchingModifier = globalScene.findModifier(
     (m) => m.isPokemonHeldItemModifier() && m.pokemonId === pokemon.id && m.matchType(modifier),
   );
   if (matchingModifier && matchingModifier.stackCount === matchingModifier.getMaxStackCount()) {
-    return t("partyUiHandler:tooManyItems", { pokemonName: getPokemonNameWithAffix(pokemon) });
+    return i18next.t("partyUiHandler:tooManyItems", { pokemonName: getPokemonNameWithAffix(pokemon) });
   }
   return null;
 };
