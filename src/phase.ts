aeb4a075eedeaddd1bbc524b6afc0a63c859d5a3
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-02-09 19:34:07 -0800

    fix comment in `phase.ts`

--- src/phase.ts
+++ src/phase.ts
@@ -2,7 +2,7 @@ import { globalScene } from "#app/global-scene";
 import { PhaseId } from "#enums/phase-id";
 
 export abstract class Phase {
-  /** The identifier of the phase. Unique per phase, but **not* per instance! */
+  /** The identifier of the phase. Unique per phase, but **not** per instance! */
   public readonly id: PhaseId = PhaseId.UNSPECIFIED;
 
   public start(): void {
