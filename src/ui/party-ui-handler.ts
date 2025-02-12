b6e03382abaca8a93b4d0c384265bf34acb59120
Moka <millennium.stitcher@gmail.com>
2025-02-12 18:29:10 +0100

    fix can't release pokemon message

--- src/ui/party-ui-handler.ts
+++ src/ui/party-ui-handler.ts
@@ -1153,7 +1153,7 @@ export default class PartyUiHandler extends MessageUiHandler {
         true,
       );
     } else {
-      this.showText(i18next.t("partyUihandler:cannotReleasePokemon"), null, () => this.showText("", 0), null, true);
+      this.showText(i18next.t("partyUiHandler:cannotReleasePokemon"), null, () => this.showText("", 0), null, true);
     }
   }
 
