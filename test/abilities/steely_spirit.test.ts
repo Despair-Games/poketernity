357231947d08830f23e9647eb67704e32fd71d8f
Mumble <171087428+frutescens@users.noreply.github.com>
2025-02-12 21:26:00 -0800

    Update test/abilities/steely_spirit.test.ts
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>

--- test/abilities/steely_spirit.test.ts
+++ test/abilities/steely_spirit.test.ts
@@ -1,5 +1,4 @@
-import { allAbilities } from "#app/data/data-lists";
-import { allMoves } from "#app/data/data-lists";
+import { allAbilities, allMoves } from "#app/data/data-lists";
 import { Abilities } from "#enums/abilities";
 import { MoveId } from "#enums/move-id";
 import { Species } from "#enums/species";
