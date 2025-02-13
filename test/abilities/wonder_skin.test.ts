b750acbbf107fbdddccaecd4001cb539cac5d9cf
Mumble <171087428+frutescens@users.noreply.github.com>
2025-02-12 21:25:35 -0800

    Update test/abilities/wonder_skin.test.ts
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>

--- test/abilities/wonder_skin.test.ts
+++ test/abilities/wonder_skin.test.ts
@@ -1,5 +1,4 @@
-import { allAbilities } from "#app/data/data-lists";
-import { allMoves } from "#app/data/data-lists";
+import { allAbilities, allMoves } from "#app/data/data-lists";
 import { MoveEffectPhase } from "#app/phases/move-effect-phase";
 import { Abilities } from "#enums/abilities";
 import { MoveId } from "#enums/move-id";
