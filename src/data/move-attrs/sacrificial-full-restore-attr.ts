6065c63cd89ae19653d9af505a2534c003a07449
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-02-13 23:01:57 -0500

    Add condition

--- src/data/move-attrs/sacrificial-full-restore-attr.ts
+++ src/data/move-attrs/sacrificial-full-restore-attr.ts
@@ -28,7 +28,7 @@ export class SacrificialFullRestoreAttr extends SacrificialAttr {
   override applyEffect(user: Pokemon, target: Pokemon, move: Move): boolean {
     // We don't know which party member will be chosen, so pick the highest max HP in the party
 
-    const party = user instanceof PlayerPokemon ? globalScene.getPlayerParty() : globalScene.getEnemyParty();
+    const party = user.getParty();
 
     const maxPartyMemberHp = party
       .map((p) => p.getMaxHp())
