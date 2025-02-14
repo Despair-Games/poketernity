25931957566d541f55a21a2ec1412d79c5af0d47
NightKev <34855794+DayKev@users.noreply.github.com>
2025-02-14 14:12:53 -0800

    Use `.resetStatus()` in `Pokemon.faint()`
    Co-authored-by: Moka <54149968+MokaStitcher@users.noreply.github.com>

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -512,7 +512,7 @@ export abstract class Pokemon extends Phaser.GameObjects.Container {
    */
   public faint(): void {
     this.hp = 0;
-    this.status = null;
+    this.resetStatus(true);
   }
 
   /**
