83b2d9c5a714268f01edb547260ae270764ad0dd
frutescens <info@laptop>
2025-02-18 11:45:58 -0800

    The ability 'Healer' uses Wave-RNG

--- src/data/init-abilities.ts
+++ src/data/init-abilities.ts
@@ -627,7 +627,7 @@ export function initAbilities() {
       .condition((pokemon) => pokemon.getHpRatio() <= 0.5),
     new Ability(Abilities.CURSED_BODY, 5).attr(PostDefendMoveDisableAbAttr, 30).bypassFaint(),
     new Ability(Abilities.HEALER, 5).conditionalAttr(
-      (pokemon) => pokemon.getAlly() && randSeedInt(10) < 3,
+      (pokemon) => pokemon.getAlly() && pokemon.randSeedInt(10) < 3,
       PostTurnResetStatusAbAttr,
       true,
     ),
