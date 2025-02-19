db8a069bf3e9eac6779853630e5848885442f1ef
frutescens <info@laptop>
2025-02-19 13:21:29 -0800

    one last

--- src/phases/post-turn-status-effect-phase.ts
+++ src/phases/post-turn-status-effect-phase.ts
@@ -43,7 +43,7 @@ export class PostTurnStatusEffectPhase extends PokemonPhase {
             damage.value = Math.max(pokemon.getMaxHp() >> 3, 1);
             break;
           case StatusEffect.TOXIC:
-            damage.value = Math.max(Math.floor((pokemon.getMaxHp() / 16) * pokemon.status.toxicTurnCount), 1);
+            damage.value = Math.max(Math.floor((pokemon.getMaxHp() / 16) * pokemon.status!.toxicTurnCount), 1);
             break;
           case StatusEffect.BURN:
             damage.value = Math.max(pokemon.getMaxHp() >> 4, 1);
