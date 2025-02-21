1540ae83cb4dfeb05133d15922f7c14bcb805a0c
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-02-21 00:50:00 -0800

    Rewrite Rollout test

--- test/moves/rollout.test.ts
+++ test/moves/rollout.test.ts
@@ -1,6 +1,7 @@
 import { allMoves } from "#app/data/data-lists";
-import { CommandPhase } from "#app/phases/command-phase";
 import { Abilities } from "#enums/abilities";
+import { BattlerIndex } from "#enums/battler-index";
+import { BattlerTagType } from "#enums/battler-tag-type";
 import { MoveId } from "#enums/move-id";
 import { Species } from "#enums/species";
 import { GameManager } from "#test/testUtils/gameManager";
@@ -23,57 +24,90 @@ describe("Moves - Rollout", () => {
 
   beforeEach(() => {
     game = new GameManager(phaserGame);
-    game.override.disableCrits();
-    game.override.battleType("single");
-    game.override.starterSpecies(Species.RATTATA);
-    game.override.ability(Abilities.BALL_FETCH);
-    game.override.enemySpecies(Species.BIDOOF);
-    game.override.enemyAbility(Abilities.BALL_FETCH);
-    game.override.startingLevel(100);
-    game.override.enemyLevel(100);
-    game.override.enemyMoveset(MoveId.SPLASH);
+    game.override
+      .disableCrits()
+      .battleType("single")
+      .ability(Abilities.BALL_FETCH)
+      .enemySpecies(Species.STAKATAKA)
+      .enemyAbility(Abilities.BALL_FETCH)
+      .startingLevel(100)
+      .enemyLevel(100)
+      .enemyMoveset(MoveId.SPLASH);
   });
 
-  it("should double it's dmg on sequential uses but reset after 5", async () => {
-    game.override.moveset([MoveId.ROLLOUT]);
-    vi.spyOn(allMoves[MoveId.ROLLOUT], "accuracy", "get").mockReturnValue(100); //always hit
+  it("should double its power on sequential uses for up to 5 uses", async () => {
+    const moveObj = allMoves[MoveId.ROLLOUT];
+    vi.spyOn(moveObj, "accuracy", "get").mockReturnValue(100); //always hit
+    const spy = vi.spyOn(moveObj, "calculateBattlePower");
 
-    const variance = 5;
-    const turns = 6;
-    const dmgHistory: number[] = [];
+    await game.classicMode.startBattle([Species.FEEBAS]);
 
-    await game.startBattle();
+    const powerResults: number[] = [];
 
-    const playerPkm = game.scene.getPlayerParty()[0];
-    vi.spyOn(playerPkm, "stats", "get").mockReturnValue([500000, 1, 1, 1, 1, 1]); // HP, ATK, DEF, SPATK, SPDEF, SPD
+    for (let i = 0; i < 6; i++) {
+      game.move.use(MoveId.ROLLOUT);
+      await game.toNextTurn();
 
-    const enemyPkm = game.scene.getEnemyParty()[0];
-    vi.spyOn(enemyPkm, "stats", "get").mockReturnValue([500000, 1, 1, 1, 1, 1]); // HP, ATK, DEF, SPATK, SPDEF, SPD
-    vi.spyOn(enemyPkm, "getHeldItems").mockReturnValue([]); //no berries
-
-    enemyPkm.hp = enemyPkm.getMaxHp();
-    let previousHp = enemyPkm.hp;
-
-    for (let i = 0; i < turns; i++) {
-      game.move.select(MoveId.ROLLOUT);
-      await game.phaseInterceptor.to(CommandPhase);
-
-      dmgHistory.push(previousHp - enemyPkm.hp);
-      previousHp = enemyPkm.hp;
+      const lastReturnedPower = spy.mock.results.at(-1)?.value;
+      if (lastReturnedPower) {
+        powerResults.push(lastReturnedPower);
+      }
     }
 
-    const [turn1Dmg, turn2Dmg, turn3Dmg, turn4Dmg, turn5Dmg, turn6Dmg] = dmgHistory;
+    expect(powerResults).toHaveLength(6);
+    powerResults
+      .slice(1, 5)
+      .forEach((power, i) => expect(power).toBe(Math.floor(Math.pow(2, i + 1) * powerResults[0])));
+    expect(powerResults[5]).toBe(powerResults[0]);
+  });
 
-    expect(turn2Dmg).toBeGreaterThanOrEqual(turn1Dmg * 2 - variance);
-    expect(turn2Dmg).toBeLessThanOrEqual(turn1Dmg * 2 + variance);
-    expect(turn3Dmg).toBeGreaterThanOrEqual(turn2Dmg * 2 - variance);
-    expect(turn3Dmg).toBeLessThanOrEqual(turn2Dmg * 2 + variance);
-    expect(turn4Dmg).toBeGreaterThanOrEqual(turn3Dmg * 2 - variance);
-    expect(turn4Dmg).toBeLessThanOrEqual(turn3Dmg * 2 + variance);
-    expect(turn5Dmg).toBeGreaterThanOrEqual(turn4Dmg * 2 - variance);
-    expect(turn5Dmg).toBeLessThanOrEqual(turn4Dmg * 2 + variance);
-    // reset
-    expect(turn6Dmg).toBeGreaterThanOrEqual(turn1Dmg - variance);
-    expect(turn6Dmg).toBeLessThanOrEqual(turn1Dmg + variance);
+  it("should lock the user into the move for 5 turns", async () => {
+    vi.spyOn(allMoves[MoveId.ROLLOUT], "accuracy", "get").mockReturnValue(100);
+
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    const player = game.field.getPlayerPokemon();
+
+    game.move.use(MoveId.ROLLOUT);
+    await game.toNextTurn();
+
+    // For the next 4 turns, check that the move is queued automatically
+    // and that the move-locking tag's data is updated correctly
+    for (let i = 0; i < 4; i++) {
+      expect(player.getTag(BattlerTagType.ROLLING)?.turnCount).toBe(4 - i);
+      expect(player.getMoveQueue()[0]).toMatchObject({
+        moveId: MoveId.ROLLOUT,
+        ignorePP: true,
+      });
+
+      await game.toNextTurn();
+    }
+
+    // The move-locking tag should be removed after the move fully executes
+    expect(player.getTag(BattlerTagType.ROLLING)).toBeUndefined();
+    expect(player.getMoveQueue()).toHaveLength(0);
+  });
+
+  it("should stop its execution if an attack is unsuccessful", async () => {
+    await game.classicMode.startBattle([Species.FEEBAS]);
+
+    const player = game.field.getPlayerPokemon();
+
+    game.move.use(MoveId.ROLLOUT);
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceHit();
+
+    await game.toNextTurn();
+
+    expect(player.getTag(BattlerTagType.ROLLING)).toBeDefined();
+    expect(player.getMoveQueue()[0]?.moveId).toBe(MoveId.ROLLOUT);
+
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceMiss();
+
+    await game.toNextTurn();
+
+    expect(player.getTag(BattlerTagType.ROLLING)).toBeUndefined();
+    expect(player.getMoveQueue()).toHaveLength(0);
   });
 });
