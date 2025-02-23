3fe5dee63815486326d4c74622c31eaeb6537dab
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-02-22 22:22:29 -0500

    Add minor comment

--- src/data/battler-tags.ts
+++ src/data/battler-tags.ts
@@ -1870,6 +1870,7 @@ export class PerishSongTag extends BattlerTag {
         }),
       );
     } else {
+      // The 2 here is just a number big enough to overcome the G-Max damage reduction
       pokemon.damageAndUpdate(2 * pokemon.hp, HitResult.ONE_HIT_KO, false, true, true);
     }
 
