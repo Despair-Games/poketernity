ca37ed106ee620bdd1c69f2e76ca880372327ef5
torranx <torranicles@gmail.com>
2025-02-24 08:12:06 +0800

    add docs

--- src/audioManager.ts
+++ src/audioManager.ts
@@ -7,6 +7,9 @@ import type BattleScene from "./battle-scene";
 
 export type AnySound = Phaser.Sound.WebAudioSound | Phaser.Sound.HTML5AudioSound | Phaser.Sound.NoAudioSound;
 
+/**
+ * Manages audio playback in the game.
+ */
 export class AudioManager {
   private bgm: AnySound;
   private bgmResumeTimer: Phaser.Time.TimerEvent | null;
@@ -18,7 +21,10 @@ export class AudioManager {
     this.scene = scene;
   }
 
-  updateSoundVolume(): void {
+  /**
+   * Updates the volume of a sound based on its key.
+   */
+  public updateSoundVolume(): void {
     if (this.scene.sound) {
       for (const sound of this.scene.sound.getAllPlaying() as AnySound[]) {
         if (this.bgmCache.has(sound.key)) {
@@ -43,9 +49,14 @@ export class AudioManager {
     }
   }
 
-  playSound(sound: string | AnySound, config?: object): AnySound {
+  /**
+   * Plays a sound effect with the given key.
+   * @param sound - The sound effect to play.
+   * @param config - Configuration options for the sound effect.
+   * @returns The sound effect that was played.
+   */
+  public playSound(sound: string | AnySound, config: object = {}): AnySound {
     const key = typeof sound === "string" ? sound : sound.key;
-    config = config ?? {};
     try {
       const keyDetails = key.split("/");
       config["volume"] = config["volume"] ?? 1;
@@ -84,15 +95,21 @@ export class AudioManager {
     }
   }
 
-  playSoundWithoutBgm(soundName: string, pauseDuration?: number): AnySound {
+  /**
+   * Plays a sound effect with the given key, without affecting the current BGM.
+   * @param soundName - The name of the sound effect to play.
+   * @param pauseDuration - The duration to pause the BGM for, in milliseconds.
+   * @returns The sound effect that was played.
+   */
+  public playSoundWithoutBgm(soundName: string, pauseDuration?: number): AnySound {
     this.bgmCache.add(soundName);
-    const resumeBgm = this.pauseBgm();
+    const isBgmPaused = this.pauseBgm();
     this.playSound(soundName);
     const sound = this.scene.sound.get(soundName) as AnySound;
     if (this.bgmResumeTimer) {
       this.bgmResumeTimer.destroy();
     }
-    if (resumeBgm) {
+    if (isBgmPaused) {
       this.bgmResumeTimer = this.scene.time.delayedCall(
         pauseDuration || fixedNumber(sound.totalDuration * 1000),
         () => {
@@ -104,23 +121,13 @@ export class AudioManager {
     return sound;
   }
 
-  pauseBgm(): boolean {
-    if (this.bgm && !this.bgm.pendingRemove && this.bgm.isPlaying) {
-      this.bgm.pause();
-      return true;
-    }
-    return false;
-  }
-
-  resumeBgm(): boolean {
-    if (this.bgm && !this.bgm.pendingRemove && this.bgm.isPaused) {
-      this.bgm.resume();
-      return true;
-    }
-    return false;
-  }
-
-  fadeOutBgm(duration: number = 500, destroy: boolean = true): boolean {
+  /**
+   * Fades out the current BGM track.
+   * @param duration - The duration of the fade out, in milliseconds.
+   * @param destroy - If `true`, the BGM will be destroyed after fading out.
+   * @returns `true` if the BGM was faded out, `false` otherwise.
+   */
+  public fadeOutBgm(duration: number = 500, destroy: boolean = true): boolean {
     if (!this.bgm) {
       return false;
     }
@@ -135,18 +142,23 @@ export class AudioManager {
 
   /**
    * Fades out current track for `delay` ms, then fades in new track.
-   * @param newBgmKey
-   * @param destroy
-   * @param delay
+   * @param newBgmKey - The key of the new BGM track to play.
+   * @param destroy - If `true`, the current BGM will be destroyed after fading out.
+   * @param delay - The delay in milliseconds before the new BGM starts playing.
    */
-  fadeAndSwitchBgm(newBgmKey: string, destroy: boolean = false, delay: number = 2000) {
+  public fadeAndSwitchBgm(newBgmKey: string, destroy: boolean = false, delay: number = 2000) {
     this.fadeOutBgm(delay, destroy);
     this.scene.time.delayedCall(delay, () => {
       this.playBgm(newBgmKey);
     });
   }
 
-  playBgm(bgmName?: string, fadeOut?: boolean): void {
+  /**
+   * Plays a BGM track.
+   * @param bgmName - The name of the BGM track to play. If not provided, the default BGM track for the current scene will be played.
+   * @param fadeOut - If true, the current BGM track will fade out before the new track starts playing.
+   */
+  public playBgm(bgmName?: string, fadeOut?: boolean): void {
     if (bgmName === undefined) {
       bgmName = this.scene.currentBattle?.getBgmOverride() || this.scene.arena?.bgm;
     }
@@ -204,11 +216,44 @@ export class AudioManager {
     }
   }
 
-  getBgmLoopPoint(bgmName: string): number {
+  /**
+   * Checks if a BGM track is currently playing.
+   * @returns `true` if a BGM track is playing, `false` otherwise.
+   */
+  isBgmPlaying(): boolean {
+    return this.bgm?.isPlaying;
+  }
+
+  /**
+   * Gets the {@linkcode bgmLoopPoint | loop point} of a BGM track.
+   * @param bgmName - The name of the BGM track to get the loop point for.
+   * @returns The loop point of the BGM track, or 0 if no loop point is set.
+   */
+  private getBgmLoopPoint(bgmName: string): number {
     return bgmLoopPoint[bgmName] ?? 0;
   }
 
-  isBgmPlaying(): boolean {
-    return this.bgm && this.bgm.isPlaying;
+  /**
+   * Pauses the current BGM track.
+   * @returns `true` if the BGM was paused, `false` otherwise.
+   */
+  private pauseBgm(): boolean {
+    if (this.bgm && !this.bgm.pendingRemove && this.bgm.isPlaying) {
+      this.bgm.pause();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Resumes the current BGM track.
+   * @returns `true` if the BGM was resumed, `false` otherwise.
+   */
+  private resumeBgm(): boolean {
+    if (this.bgm && !this.bgm.pendingRemove && this.bgm.isPaused) {
+      this.bgm.resume();
+      return true;
+    }
+    return false;
   }
 }
