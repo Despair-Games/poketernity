05c7d913e7f330e03010ecabaa2a429b4d1ff134
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-03-06 22:46:55 -0800

    Lightning Rod and Storm Drain now redirect based on final move type

---
+++ test/abilities/lightning_rod.test.ts
@@ -0,0 +1,85 @@
+import { allAbilities } from "#app/data/data-lists";
+import { Abilities } from "#enums/abilities";
+import { BattlerIndex } from "#enums/battler-index";
+import { MoveId } from "#enums/move-id";
+import { Species } from "#enums/species";
+import { Stat } from "#enums/stat";
+import { GameManager } from "#test/testUtils/gameManager";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+
+describe("Abilities - Lightning Rod", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .ability(Abilities.BALL_FETCH)
+      .battleType("double")
+      .disableCrits()
+      .enemySpecies(Species.MAGIKARP)
+      .enemyAbility(Abilities.BALL_FETCH)
+      .enemyMoveset(MoveId.SPLASH)
+      .startingLevel(100)
+      .enemyLevel(100);
+  });
+
+  it("should redirect single-target moves", async () => {
+    await game.classicMode.startBattle([Species.FEEBAS, Species.MAGIKARP]);
+
+    const enemyPokemon = game.scene.getEnemyField();
+
+    vi.spyOn(enemyPokemon[0], "getAbility").mockReturnValue(allAbilities[Abilities.LIGHTNING_ROD]);
+
+    game.move.use(MoveId.THUNDER_SHOCK, 0, BattlerIndex.ENEMY_2);
+    game.move.use(MoveId.SPLASH, 1);
+    await game.toEndOfTurn();
+
+    enemyPokemon.forEach((p) => expect(p.isFullHp()).toBeTruthy());
+    expect(enemyPokemon[0].getStatStage(Stat.SPATK)).toBe(1);
+  });
+
+  it("should not redirect multi-target moves", async () => {
+    await game.classicMode.startBattle([Species.FEEBAS, Species.MAGIKARP]);
+
+    const enemyPokemon = game.scene.getEnemyField();
+
+    vi.spyOn(enemyPokemon[0], "getAbility").mockReturnValue(allAbilities[Abilities.LIGHTNING_ROD]);
+
+    game.move.use(MoveId.DISCHARGE, 0);
+    game.move.use(MoveId.SPLASH, 1);
+    await game.toEndOfTurn();
+
+    expect(enemyPokemon[0].isFullHp()).toBeTruthy();
+    expect(enemyPokemon[1].isFullHp()).toBeFalsy();
+    expect(enemyPokemon[0].getStatStage(Stat.SPATK)).toBe(1);
+  });
+
+  it("should not redirect moves boosted by Normalize", async () => {
+    game.override.ability(Abilities.NORMALIZE);
+
+    await game.classicMode.startBattle([Species.FEEBAS, Species.MAGIKARP]);
+
+    const enemyPokemon = game.scene.getEnemyField();
+
+    vi.spyOn(enemyPokemon[0], "getAbility").mockReturnValue(allAbilities[Abilities.LIGHTNING_ROD]);
+
+    game.move.use(MoveId.THUNDER_SHOCK, 0, BattlerIndex.ENEMY_2);
+    game.move.use(MoveId.SPLASH, 1);
+    await game.toEndOfTurn();
+
+    expect(enemyPokemon[1].isFullHp()).toBeFalsy();
+    expect(enemyPokemon[0].getStatStage(Stat.SPATK)).toBe(0);
+  });
+});
