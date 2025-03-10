00b15dd44e6c5b6799ecfa6ea8640b6e1ac7e7bb
NightKev <34855794+DayKev@users.noreply.github.com>
2025-03-10 02:19:21 -0700

    Extend `Map` with `MoveMap` to prevent the need to add `!` everywhere

--- src/data/data-lists.ts
+++ src/data/data-lists.ts
@@ -1,9 +1,14 @@
-import type PokemonSpecies from "#app/data/pokemon-species";
+import { type Ability } from "#app/data/ability";
 import { type Move } from "#app/data/move";
+import type PokemonSpecies from "#app/data/pokemon-species";
 import { type MoveId } from "#enums/move-id";
-import type { Ability } from "#app/data/ability";
+
+interface MoveMap<K, V> extends Map<K, V> {
+  get(key: K): V;
+}
 
 // Initialized as being empty; these will be filled during initialization
 export const allSpecies: PokemonSpecies[] = [];
-export const allMoves = new Map<MoveId, Move>();
+// @ts-expect-error - this forcibly overrides `Map`'s `get` function to not type hint a return of `undefined`
+export const allMoves: MoveMap<MoveId, Move> = new Map<MoveId, Move>();
 export const allAbilities: Ability[] = [];
