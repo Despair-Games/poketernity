07301f2843b4d87d07735a100f5d0f473c64cbab
NightKev <34855794+DayKev@users.noreply.github.com>
2025-03-09 17:35:28 -0700

    [Refactor] Use a `Map` instead of an object for `allMoves`

--- src/data/data-lists.ts
+++ src/data/data-lists.ts
@@ -3,13 +3,7 @@ import { type Move } from "#app/data/move";
 import { type MoveId } from "#enums/move-id";
 import type { Ability } from "#app/data/ability";
 
-//#region Type
-
-export type AllMoves = {
-  [moveId in MoveId]: Move;
-};
-
 // Initialized as being empty; these will be filled during initialization
 export const allSpecies: PokemonSpecies[] = [];
-export const allMoves: AllMoves = {} as AllMoves;
+export const allMoves = new Map<MoveId, Move>();
 export const allAbilities: Ability[] = [];
