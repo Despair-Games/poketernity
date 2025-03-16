b5173fa30d02b95d1a2293d97a0f1b13039c9670
torranx <torranicles@gmail.com>
2025-03-17 05:16:30 +0800

    add subfolders to glob pattern

--- test/vitest.setup.ts
+++ test/vitest.setup.ts
@@ -35,7 +35,7 @@ vi.mock("i18next", async (importOriginal) => {
       const filename = req.params[0];
 
       try {
-        const localeFiles = import.meta.glob("../public/locales/en/*.json", { eager: true });
+        const localeFiles = import.meta.glob("../public/locales/en/**/*.json", { eager: true });
         const json = localeFiles[`../public/locales/en/${filename}`] || {};
         console.log("Loaded locale", filename);
         return HttpResponse.json(json);
