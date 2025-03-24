bd9b7100e0ce0fc81f0296df0290dcb6840f2971
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-03-23 22:13:28 -0400

    Add in filler biomes used in unit tests

--- src/data/init/init-biomes.ts
+++ src/data/init/init-biomes.ts
@@ -32,7 +32,22 @@ export function initBiomes() {
       "volcano",
     ),
     new PEBiome(Biome.CAVE, cavePokemonPool, caveTrainerPool, caveWeatherPool, caveTerrainPool, "cave"),
-    // The below are all filler values for now
+    // The below are all filler values for now, used in certain unit tests
+    new PEBiome(Biome.MOUNTAIN, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "TOWN"),
+    new PEBiome(Biome.LAKE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.ICE_CAVE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.SNOWY_FOREST, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.END, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.GRASS, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.CAVE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.PLAINS, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.SPACE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.SEA, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.SWAMP, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.JUNGLE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.LABORATORY, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.FAIRY_CAVE, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
+    new PEBiome(Biome.WASTELAND, townPokemonPool, townTrainerPool, townWeatherPool, townTerrainPool, "town"),
   ];
 
   for (const pebiome of rawAllBiomes) {
