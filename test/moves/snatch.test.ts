a7488a4b1de737dcb9018b454795cb102278dc48
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-03-25 00:38:29 -0700

    Fix conflicts in Snatch test

--- test/moves/snatch.test.ts
+++ test/moves/snatch.test.ts
@@ -1,9 +1,9 @@
 import type { StockpilingTag } from "#app/data/battler-tags/stockpiling-tag";
-import { Abilities } from "#enums/abilities";
+import { AbilityId } from "#enums/ability-id";
 import { BattlerIndex } from "#enums/battler-index";
 import { BattlerTagType } from "#enums/battler-tag-type";
 import { MoveId } from "#enums/move-id";
-import { Species } from "#enums/species";
+import { SpeciesId } from "#enums/species-id";
 import { Stat } from "#enums/stat";
 import { StatusEffect } from "#enums/status-effect";
 import { GameManager } from "#test/test-utils/gameManager";
@@ -27,18 +27,18 @@ describe("Moves - Snatch", () => {
   beforeEach(() => {
     game = new GameManager(phaserGame);
     game.override
-      .ability(Abilities.BALL_FETCH)
+      .ability(AbilityId.BALL_FETCH)
       .battleType("single")
       .disableCrits()
-      .enemySpecies(Species.MAGIKARP)
-      .enemyAbility(Abilities.BALL_FETCH)
+      .enemySpecies(SpeciesId.MAGIKARP)
+      .enemyAbility(AbilityId.BALL_FETCH)
       .enemyMoveset(MoveId.SWORDS_DANCE)
       .startingLevel(100)
       .enemyLevel(100);
   });
 
   it("should steal the effects of the next used beneficial status move in the turn", async () => {
-    await game.classicMode.startBattle([Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
     const player = game.field.getPlayerPokemon();
     const enemy = game.field.getEnemyPokemon();
@@ -53,7 +53,7 @@ describe("Moves - Snatch", () => {
   it("should only steal the first snatchable move from another Pokemon", async () => {
     game.override.battleType("double");
 
-    await game.classicMode.startBattle([Species.FEEBAS, Species.MAGIKARP]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS, SpeciesId.MAGIKARP]);
 
     const playerPokemon = game.scene.getPlayerField();
     const enemyPokemon = game.scene.getEnemyField();
@@ -73,7 +73,7 @@ describe("Moves - Snatch", () => {
     { moveId: MoveId.REST, moveName: "Rest" },
     { moveId: MoveId.SWALLOW, moveName: "Swallow" },
   ])("should not steal $moveName when it has no effect on the original user", async ({ moveId }) => {
-    await game.classicMode.startBattle([Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
     const player = game.field.getPlayerPokemon();
     const enemy = game.field.getEnemyPokemon();
@@ -93,7 +93,7 @@ describe("Moves - Snatch", () => {
   });
 
   it("should steal Rest if it would affect the original user", async () => {
-    await game.classicMode.startBattle([Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
     const player = game.field.getPlayerPokemon();
     const enemy = game.field.getEnemyPokemon();
@@ -113,7 +113,7 @@ describe("Moves - Snatch", () => {
   });
 
   it("should steal Swallow if the original user has Stockpiled, restoring 25% HP", async () => {
-    await game.classicMode.startBattle([Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
     const player = game.field.getPlayerPokemon();
     const enemy = game.field.getEnemyPokemon();
@@ -141,7 +141,7 @@ describe("Moves - Snatch", () => {
   it("should only activate for the first Pokemon to use Snatch", async () => {
     game.override.battleType("double");
 
-    await game.classicMode.startBattle([Species.MAGIKARP, Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.MAGIKARP, SpeciesId.FEEBAS]);
 
     game.setTurnOrder([BattlerIndex.ENEMY_2, BattlerIndex.ENEMY, BattlerIndex.PLAYER_2, BattlerIndex.PLAYER]);
     game.move.use(MoveId.SNATCH, 0);
@@ -163,7 +163,7 @@ describe("Moves - Snatch", () => {
   it("should not activate if its user is picked up by Sky Drop", async () => {
     game.override.battleType("double");
 
-    await game.classicMode.startBattle([Species.MAGIKARP, Species.FEEBAS]);
+    await game.classicMode.startBattle([SpeciesId.MAGIKARP, SpeciesId.FEEBAS]);
 
     game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY, BattlerIndex.PLAYER_2, BattlerIndex.ENEMY_2]);
     game.move.use(MoveId.SNATCH, 0);
