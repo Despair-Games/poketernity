40a749e5a4d281e2e927c9e8952e9c98b4cdad60
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-04-04 09:04:45 -0700

    delete Range type

--- src/@types/Range.ts
+++
@@ -1,7 +0,0 @@
-// source: https://stackoverflow.com/a/70307091
-
-type Enumerate<N extends number, Acc extends number[] = []> = Acc["length"] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc["length"]]>;
-
-export type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
