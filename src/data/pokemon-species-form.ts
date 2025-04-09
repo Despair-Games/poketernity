c9b18580eb05f9b6871b922ad49242e24febab13
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-09 04:19:56 -0700

    Update imports in `pokemon-species-form.ts`

--- src/data/pokemon-species-form.ts
+++ src/data/pokemon-species-form.ts
@@ -1,11 +1,12 @@
 import type { StarterMoveset } from "#app/@types/StarterData";
 import type { AnySound } from "#app/audio-manager";
-import { type LevelMoves, pokemonSpeciesLevelMoves } from "#app/data/pokemon-level-moves";
-import { speciesStarterCosts } from "#app/data/starters";
 import { speciesEggMoves } from "#app/data/egg-moves";
 import { noRandomSpeciesSpawn } from "#app/data/no-random-species-spawn";
+import type { PokemonForm } from "#app/data/pokemon-form";
 import { pokemonFormLevelMoves } from "#app/data/pokemon-form-level-moves";
+import { type LevelMoves, pokemonSpeciesLevelMoves } from "#app/data/pokemon-level-moves";
 import { pokemonPreEvolutions } from "#app/data/pokemon-pre-evolutions";
+import { speciesStarterCosts } from "#app/data/starters";
 import { type Variant, variantData, type VariantSet } from "#app/data/variant";
 import { globalScene } from "#app/global-scene";
 import { getPokemonSpecies } from "#app/utils/pokemon-species-utils";
@@ -16,7 +17,6 @@ import { SpeciesFormKey } from "#enums/species-form-key";
 import { SpeciesId } from "#enums/species-id";
 import type { Stat } from "#enums/stat";
 import { argbFromRgba, QuantizerCelebi, rgbaFromArgb } from "@material/material-color-utilities";
-import type { PokemonForm } from "./pokemon-form";
 
 //#region Types
 
