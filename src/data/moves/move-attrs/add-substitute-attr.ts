415ad127dd54a2ea933adc86634a994c0611c4c1
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-04-11 20:33:44 -0400

    Update add-substitute-attr

--- src/data/moves/move-attrs/add-substitute-attr.ts
+++ src/data/moves/move-attrs/add-substitute-attr.ts
@@ -15,17 +15,22 @@ import i18next from "i18next";
  * @see {@linkcode apply}
  */
 export class AddSubstituteAttr extends MoveEffectAttr {
-  /** The ratio of the user's max HP that is required to apply this effect */
-  private hpCost: number;
+  /**
+   * This attr is only used for Substitute and Shed Tail.
+   * Substitute costs 1/4 of the user's max hp and rounds down
+   * Shed tail costs 1/2 of the user's max hp and rounds up
+   */
+  private forShedTail: boolean;
 
-  constructor(hpCost: number = 0.25) {
+  constructor(forShedTail: boolean = false) {
     super(true);
 
-    this.hpCost = hpCost;
+    this.forShedTail = forShedTail;
   }
 
   override applyEffect(user: Pokemon, _target: Pokemon, move: Move): boolean {
-    user.damageAndUpdate(Math.floor(user.getMaxHp() * this.hpCost), {
+    const hpCost = this.forShedTail ? Math.ceil(user.getMaxHp() * 0.5) : Math.floor(user.getMaxHp() * 0.25);
+    user.damageAndUpdate(hpCost, {
       result: HitResult.OTHER,
       ignoreSegments: true,
       preventEndure: true,
@@ -42,9 +47,15 @@ export class AddSubstituteAttr extends MoveEffectAttr {
   }
 
   override getCondition(): MoveConditionFunc {
+    /**
+     * Only works if
+     * - The user does not have a substitute out already
+     * - The user has enough HP
+     * - THe user has more than 1 max hp (wonder guard users)
+     */
     return (user, _target, _move) =>
       !user.getTag(BattlerTagType.SUBSTITUTE)
-      && user.hp > Math.floor(user.getMaxHp() * this.hpCost)
+      && user.hp > (this.forShedTail ? Math.ceil(user.getMaxHp() * 0.5) : Math.floor(user.getMaxHp() * 0.25))
       && user.getMaxHp() > 1;
   }
 
