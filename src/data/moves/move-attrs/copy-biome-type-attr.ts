fc900c652486f99c8bf6dbb69c80a05149644719
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-04-13 18:50:33 -0400

    Keep specific move biomes in their own files for now

--- src/data/moves/move-attrs/copy-biome-type-attr.ts
+++ src/data/moves/move-attrs/copy-biome-type-attr.ts
@@ -6,7 +6,7 @@ import { getPokemonNameWithAffix } from "#app/messages";
 import i18next from "i18next";
 import type { Move } from "#app/data/moves/move";
 import { MoveEffectAttr } from "#app/data/moves/move-attrs/move-effect-attr";
-import { getTypeForBiome } from "#app/data/biome-utils";
+import { BiomeId } from "#enums/biome-id";
 
 /**
  * Attribute to change the user's type based on the current biome.
@@ -25,7 +25,7 @@ export class CopyBiomeTypeAttr extends MoveEffectAttr {
     if (terrainType !== TerrainType.NONE) {
       typeChange = this.getTypeForTerrain(globalScene.arena.getTerrainType());
     } else {
-      typeChange = getTypeForBiome(globalScene.arena.biomeId);
+      typeChange = this.getTypeForBiome(globalScene.arena.biomeId);
     }
 
     user.summonData.types = [typeChange];
@@ -57,6 +57,73 @@ export class CopyBiomeTypeAttr extends MoveEffectAttr {
       case TerrainType.PSYCHIC:
         return ElementalType.PSYCHIC;
       case TerrainType.NONE:
+      default:
+        return ElementalType.UNKNOWN;
+    }
+  }
+
+  /**
+   * Retrieves a type from the current biome. Used for the move Camoflage
+   * @param biomeId {@linkcode BiomeId}
+   * @returns the {@linkcode ElementalType} corresponding to the biome
+   */
+  private getTypeForBiome(biomeId: BiomeId): ElementalType {
+    switch (biomeId) {
+      case BiomeId.TOWN:
+      case BiomeId.PLAINS:
+      case BiomeId.METROPOLIS:
+        return ElementalType.NORMAL;
+      case BiomeId.GRASS:
+      case BiomeId.TALL_GRASS:
+        return ElementalType.GRASS;
+      case BiomeId.FOREST:
+      case BiomeId.JUNGLE:
+        return ElementalType.BUG;
+      case BiomeId.SLUM:
+      case BiomeId.SWAMP:
+        return ElementalType.POISON;
+      case BiomeId.SEA:
+      case BiomeId.BEACH:
+      case BiomeId.LAKE:
+      case BiomeId.SEABED:
+        return ElementalType.WATER;
+      case BiomeId.MOUNTAIN:
+        return ElementalType.FLYING;
+      case BiomeId.BADLANDS:
+        return ElementalType.GROUND;
+      case BiomeId.CAVE:
+      case BiomeId.DESERT:
+        return ElementalType.ROCK;
+      case BiomeId.ICE_CAVE:
+      case BiomeId.SNOWY_FOREST:
+        return ElementalType.ICE;
+      case BiomeId.MEADOW:
+      case BiomeId.FAIRY_CAVE:
+      case BiomeId.ISLAND:
+        return ElementalType.FAIRY;
+      case BiomeId.POWER_PLANT:
+      case BiomeId.CHARGESTONE_CAVE:
+        return ElementalType.ELECTRIC;
+      case BiomeId.VOLCANO:
+      case BiomeId.STEAM_VENT:
+        return ElementalType.FIRE;
+      case BiomeId.GRAVEYARD:
+      case BiomeId.TEMPLE:
+        return ElementalType.GHOST;
+      case BiomeId.DOJO:
+      case BiomeId.CONSTRUCTION_SITE:
+        return ElementalType.FIGHTING;
+      case BiomeId.FACTORY:
+      case BiomeId.LABORATORY:
+        return ElementalType.STEEL;
+      case BiomeId.RUINS:
+      case BiomeId.SPACE:
+        return ElementalType.PSYCHIC;
+      case BiomeId.WASTELAND:
+      case BiomeId.END:
+        return ElementalType.DRAGON;
+      case BiomeId.ABYSS:
+        return ElementalType.DARK;
       default:
         return ElementalType.UNKNOWN;
     }
