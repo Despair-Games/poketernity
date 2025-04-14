92217a3df03a5e0b1faf1f5029a81165cadfbf01
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-14 05:01:16 -0700

    Fix "all moves" test

--- test/data/all-moves.test.ts
+++ test/data/all-moves.test.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { readFileSync } from "fs";
 import { describe, expect, it } from "vitest";
 import { MoveCategory } from "#enums/move-category";
-import { MoveId } from "#enums/move-id";
+import type { MoveId } from "#enums/move-id";
 import type { Move } from "#app/data/moves/move";
 import { MoveFlags } from "#enums/move-flags";
 import { ElementalType } from "#enums/elemental-type";
@@ -74,35 +74,35 @@ describe("All Moves", async () => {
   it.each(moveData)("$identifier, if implemented, should have correct move data", async (move: MoveData) => {
     const pktyMove = allMoves.get(move.id as MoveId) as Move;
     if (pktyMove && !isUnimplemented(pktyMove.name)) {
-      expect(
-        pktyMove.type,
-        `Elemntal type of ${MoveId[pktyMove.id]} should be ${ElementalType[move.type_id - 1]} but is ${ElementalType[pktyMove.type]}`,
-      ).toBe(move.type_id - 1); // PokeAPI begins its list of types with the number 1
-      expect(
-        pktyMove.accuracy,
-        `Accuracy of ${MoveId[pktyMove.id]} should be ${move.accuracy} but is ${pktyMove.accuracy}`,
-      ).toBe(move.accuracy);
-      expect(
-        pktyMove.priority,
-        `Priority of ${MoveId[pktyMove.id]} should be ${move.priority} but is ${pktyMove.priority}`,
-      ).toBe(move.priority);
-      expect(pktyMove.power, `Power of ${MoveId[pktyMove.id]} should be ${move.power} but is ${pktyMove.power}`).toBe(
-        move.power,
+      const moveName = pktyMove.name;
+      const expectedMoveType = ElementalType[move.type_id - 1];
+      const actualMoveType = ElementalType[pktyMove.type];
+      const expectedCategory = MoveCategory[move.damage_class_id];
+      const actualCategory = MoveCategory[pktyMove.category];
+      expect(pktyMove.type, `Elemntal type of ${moveName} should be ${expectedMoveType} but is ${actualMoveType}`).toBe(
+        move.type_id - 1,
+      ); // PokeAPI begins its list of types with the number 1
+      expect(pktyMove.accuracy, `Accuracy of ${moveName} should be ${move.accuracy} but is ${pktyMove.accuracy}`).toBe(
+        move.accuracy,
       );
-      expect(pktyMove.pp, `PP of ${MoveId[pktyMove.id]} should be ${move.pp} but is ${pktyMove.pp}`).toBe(move.pp);
+      expect(pktyMove.priority, `Priority of ${moveName} should be ${move.priority} but is ${pktyMove.priority}`).toBe(
+        move.priority,
+      );
+      expect(pktyMove.power, `Power of ${moveName} should be ${move.power} but is ${pktyMove.power}`).toBe(move.power);
+      expect(pktyMove.pp, `PP of ${moveName} should be ${move.pp} but is ${pktyMove.pp}`).toBe(move.pp);
       expect(
         pktyMove.category,
-        `Move category of ${MoveId[pktyMove.id]} should be ${MoveCategory[move.damage_class_id]} but is ${MoveCategory[pktyMove.category]}`,
+        `Move category of ${moveName} should be ${expectedCategory} but is ${actualCategory}`,
       ).toBe(move.damage_class_id);
-      expect(
-        pktyMove.chance,
-        `Chance of ${MoveId[pktyMove.id]} should be ${move.effect_chance} but is ${pktyMove.chance}`,
-      ).toBe(move.effect_chance);
+      expect(pktyMove.chance, `Chance of ${moveName} should be ${move.effect_chance} but is ${pktyMove.chance}`).toBe(
+        move.effect_chance,
+      );
       if (Array.isArray(move.flags)) {
         for (const f of Object.keys(flagsToCheck)) {
+          const flagName = MoveFlags[flagsToCheck[f]];
           const actualHasFlag = pktyMove.hasFlag(flagsToCheck[f]);
           const expectedHasFlag = move.flags.includes(Number(f));
-          const errOutput = `${MoveId[pktyMove.id]}'s usage of flag ${MoveFlags[flagsToCheck[f]]} should be ${expectedHasFlag} but is ${actualHasFlag}!`;
+          const errOutput = `${moveName}'s usage of flag ${flagName} should be ${expectedHasFlag} but is ${actualHasFlag}!`;
           expect(actualHasFlag, errOutput).toBe(expectedHasFlag);
         }
       }
