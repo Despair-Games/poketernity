5dcb35f1c0b809d013312224143509eedb311c26
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-04-15 08:23:11 -0700

    fix moves/substitute.test

--- test/moves/substitute.test.ts
+++ test/moves/substitute.test.ts
@@ -1,6 +1,5 @@
 import type { SubstituteTag } from "#app/data/battler-tags/substitute-tag";
 import { allMoves } from "#app/data/data-lists";
-import { StealHeldItemChanceAttr } from "#app/data/moves/move-attrs/steal-held-item-chance-attr";
 import type { CommandPhase } from "#app/phases/command-phase";
 import { TrappedBattlerTagTypes } from "#app/utils/battler-tag-type-utils";
 import { AbilityId } from "#enums/ability-id";
