8c70d2d6df8b1721d28a44a2d3c08ff8e026a6ab
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-16 00:52:05 -0700

    Fix typo
    Co-authored-by: Adrian T. <68144167+torranx@users.noreply.github.com>

--- test/matchers.setup.ts
+++ test/matchers.setup.ts
@@ -3,7 +3,7 @@ import { toHaveUsedMove } from "#test/matchers/to-have-used-move";
 import { expect } from "vitest";
 
 /**
- * Setup for for custom matchers.
+ * Setup for custom matchers.
  * Make sure to define the call signatures in {@linkcode file://./@types/vitest.d.ts} too!
  */
 
