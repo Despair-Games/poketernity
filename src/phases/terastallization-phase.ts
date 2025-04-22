9d84faa32742af72bc5aa61857dc2442f4139684
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-21 22:07:52 -0700

    Add temporary failsafe to `TerastallizationPhase` to prevent crashes
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>

--- src/phases/terastallization-phase.ts
+++ src/phases/terastallization-phase.ts
@@ -10,8 +10,8 @@ import { PhaseId } from "#enums/phase-id";
 import i18next from "i18next";
 
 export class TerastallizationPhase extends BattlePhase {
+  public override readonly id = PhaseId.TERASTALLIZATION;
   public pokemon: Pokemon;
-  override readonly id = PhaseId.TERASTALLIZATION;
 
   constructor(pokemon: Pokemon) {
     super();
@@ -21,6 +21,12 @@ export class TerastallizationPhase extends BattlePhase {
 
   public override start(): void {
     super.start();
+
+    // Failsafe to prevent a crash if the pokemon faints before the phase runs
+    // TODO: remove this when Tera phase timing is fixed
+    if (this.pokemon.isFainted()) {
+      return super.end();
+    }
 
     new CommonBattleAnim(CommonAnim.TERASTALLIZE, this.pokemon).play(false, () => {
       globalScene.phaseManager.queueMessagePhase(
