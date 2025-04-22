a34fec90a768a1ea144e9e41e8df8e10782b0920
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-21 18:13:39 -0700

    Add TSDoc to `isPokemon`

--- src/utils.ts
+++ src/utils.ts
@@ -685,6 +685,7 @@ export function deepFreeze<T>(obj: T): Readonly<T> {
   return obj;
 }
 
+/** @returns `true` if the input is a `Pokemon` object */
 export function isPokemon(data: any): data is Pokemon {
   return data.hasOwnProperty("type") && data.type === "Pokemon";
 }
