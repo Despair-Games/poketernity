fc97d4a440090bd41a60a41e9fb903f006a5d88d
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-04-25 10:07:44 -0700

    Add tsdoc to FlinchStatStageChangeAbAttr

--- src/data/abilities/ab-attrs/flinch-stat-stage-change-ab-attr.ts
+++ src/data/abilities/ab-attrs/flinch-stat-stage-change-ab-attr.ts
@@ -4,8 +4,15 @@ import { StatStageChangePhase } from "#app/phases/stat-stage-change-phase";
 import type { BattleStat } from "#enums/stat";
 import { FlinchEffectAbAttr } from "./flinch-effect-ab-attr";
 
+/**
+ * Attribute that prompts a stat stage change after the ability holder is flinched.
+ * @see {@link https://bulbapedia.bulbagarden.net/wiki/Steadfast_(Ability) Steadfast - Bulbapedia}
+ */
 export class FlinchStatStageChangeAbAttr extends FlinchEffectAbAttr {
+  /** The {@linkcode BattleStat stats} to change. */
   private readonly stats: BattleStat[];
+
+  /** The number of stages to apply to the {@linkcode stats}. */
   private readonly stages: number;
 
   constructor(stats: BattleStat[], stages: number) {
@@ -15,7 +22,7 @@ export class FlinchStatStageChangeAbAttr extends FlinchEffectAbAttr {
     this.stages = stages;
   }
 
-  override apply(pokemon: Pokemon, simulated: boolean): boolean {
+  public override apply(pokemon: Pokemon, simulated: boolean): boolean {
     if (!simulated) {
       globalScene.phaseManager.unshiftPhase(
         new StatStageChangePhase(pokemon.getBattlerIndex(), pokemon, this.stats, this.stages),
