fc0e0e8b0c5902539600c96915452de9851bb475
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2025-04-25 19:36:35 -0400

    Update exp formula

--- src/field/trainer.ts
+++ src/field/trainer.ts
@@ -248,7 +248,7 @@ export default class Trainer extends Phaser.GameObjects.Container {
    * Function to get levels for a given wave and the {@linkcode TrainerPartyTemplate}
    *
    * First the waveIndex is scaled according to {@linkcode getWaveForDifficulty} which I will call `x` here
-   * The base level is 1 + x/2 + (x^2 / 625) a quadratic function that is outpaced by y=x until around wave 310
+   * This is then plugged into the {@linkcode getWaveForDifficulty} function
    *
    * If the party member strength is below STRONG, the multiplier is scaled and a negative level offset is applied
    *
