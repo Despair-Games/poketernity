523a6dfa0b0dcf5688055d19e59701ea24c66d44
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-04-30 15:39:23 -0700

    optimize app-utils

--- test/utils/app-utils.test.ts
+++ test/utils/app-utils.test.ts
@@ -1,24 +1,17 @@
-import { beforeAll, describe, expect, it } from "vitest";
-
 import { isLandscapeMode } from "#app/utils/app-utils";
 import Phaser from "phaser";
+import { describe, expect, it } from "vitest";
 
 describe("Utils - App Utils", () => {
-  let phaserGame: Phaser.Game;
-
-  beforeAll(() => {
-    phaserGame = new Phaser.Game({
-      type: Phaser.HEADLESS,
-    });
-  });
-
   describe("isLandscapeMode", () => {
     it.each([
       { label: "Landscape", id: Phaser.Scale.Orientation.LANDSCAPE },
       { label: "Landscape Secondary", id: Phaser.Scale.Orientation.LANDSCAPE_SECONDARY },
     ])("should return true if orientation is $label", ({ id }) => {
-      phaserGame.scale.orientation = id;
-      const actualresult = isLandscapeMode(phaserGame);
+      const sceneMock = { scale: { orientation: id } as Phaser.Scale.ScaleManager };
+
+      const actualresult = isLandscapeMode(sceneMock);
+
       expect(actualresult).toBe(true);
     });
 
@@ -26,8 +19,9 @@ describe("Utils - App Utils", () => {
       { label: "Portrait", id: Phaser.Scale.Orientation.PORTRAIT },
       { label: "Portrait Secondary", id: Phaser.Scale.Orientation.PORTRAIT_SECONDARY },
     ])("should return false if orientation is $label", ({ id }) => {
-      phaserGame.scale.orientation = id;
-      const actualresult = isLandscapeMode(phaserGame);
+      const sceneMock = { scale: { orientation: id } as Phaser.Scale.ScaleManager };
+      const actualresult = isLandscapeMode(sceneMock);
+
       expect(actualresult).toBe(false);
     });
   });
