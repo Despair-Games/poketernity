baa215139da11f77d2dd7fa016d127bbab422c7e
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-02 18:46:54 -0700

    Fix imports
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -124,7 +124,7 @@ import { UI } from "#app/ui/ui";
 import { setDocumentUiTheme, updateWindowStyle } from "#app/ui/ui-theme";
 import { loadCommonAnimAssets } from "#app/utils/anim-utils";
 import { BooleanHolder, fixedNumber, getEnumValues, isNil, NumberHolder } from "#app/utils/common-utils";
-import type { AbstractConstructor } from "./@types/AbstractConstructor";
+import type { AbstractConstructor } from "#app/@types/AbstractConstructor";
 import { getModifierPoolForType } from "#app/utils/modifier-pool-utils";
 import { getModifierType } from "#app/utils/modifier-type-utils";
 import { loadMoveAnimAssets } from "#app/utils/move-anim-utils";
