b83f15094d9adec35669379bc6d4d00aa19fc002
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-06 16:59:26 -0700

    add ability-constants
    and add IGNORING_ABILITIES

---
+++ src/constants/ability-constants.ts
@@ -0,0 +1,4 @@
+import { AbilityId } from "#enums/ability-id";
+
+/** Abilities that ignore/prevent the opponent's stat changes. */
+export const IGNORING_ABILITIES = [AbilityId.MOLD_BREAKER, AbilityId.TERAVOLT, AbilityId.TURBOBLAZE];
