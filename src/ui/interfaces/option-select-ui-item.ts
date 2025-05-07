8a85598a35d933339bc6fb044717f2349d282e35
Moka <54149968+MokaStitcher@users.noreply.github.com>
2025-05-07 15:40:30 +0200

    Update src/ui/interfaces/option-select-ui-item.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/ui/interfaces/option-select-ui-item.ts
+++ src/ui/interfaces/option-select-ui-item.ts
@@ -2,6 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { BaseOptionSelectUiHandler } from "#app/ui/handlers/base-option-select-ui-handler";
 // -- end tsdoc imports --
+
 import type { OptionSelectItem } from "#app/ui/interfaces/option-select-config";
 
 /**
