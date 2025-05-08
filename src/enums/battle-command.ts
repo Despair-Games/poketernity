601f72fd92dce541aee06327746c7aaf890ada6d
NightKev <34855794+DayKev@users.noreply.github.com>
2025-05-08 03:15:32 -0700

    [Bug] Fix timing of Terastallization phase

--- src/enums/battle-command.ts
+++ src/enums/battle-command.ts
@@ -1,3 +1,14 @@
+// -- start tsdoc imports --
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { CommandPhase } from "#app/phases/command-phase";
+/* eslint-enable @typescript-eslint/no-unused-vars */
+// -- end tsdoc imports --
+
+/**
+ * Commands that can be executed from a {@linkcode CommandPhase}.
+ */
+// Note: Do not re-order this without a good reason! The cursor positioning
+// relies on their values being as they are now.
 export enum BattleCommand {
   FIGHT,
   BALL,
