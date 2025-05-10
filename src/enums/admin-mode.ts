386d9f024abaa252f394c670c4a31b97b91e0dfe
NightKev <34855794+DayKev@users.noreply.github.com>
2025-05-10 05:06:42 -0700

    Convert `AdminMode` to `const` object

--- src/enums/admin-mode.ts
+++ src/enums/admin-mode.ts
@@ -1,5 +1,7 @@
-export enum AdminMode {
-  LINK,
-  SEARCH,
-  ADMIN,
-}
+export const AdminMode = {
+  LINK: 1,
+  SEARCH: 2,
+  ADMIN: 3,
+} as const;
+
+export type AdminMode = (typeof AdminMode)[keyof typeof AdminMode];
