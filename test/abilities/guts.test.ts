7df958c0d4f7ec802ba05078bcc317f78e2fa760
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-15 20:10:21 -0700

    add guts test

---
+++ test/abilities/guts.test.ts
@@ -0,0 +1,112 @@
+import { NON_VOLATILE_STATUS_EFFECTS } from "#app/constants/game-constants";
+import { BattlerIndex } from "#app/enums/battler-index";
+import { Stat } from "#app/enums/stat";
+import { StatusEffect } from "#app/enums/status-effect";
+import { capitalizeString } from "#app/utils/string-utils";
+import { AbilityId } from "#enums/ability-id";
+import { MoveId } from "#enums/move-id";
+import { SpeciesId } from "#enums/species-id";
+import { GameManager } from "#test/test-utils/game-manager";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+//#region Test Constants
+
+const nonVolatileStatusEffects = NON_VOLATILE_STATUS_EFFECTS.map((statusEffectId) => ({
+  statusEffectName: capitalizeString(StatusEffect[statusEffectId], "_", false, true),
+  statusEffectId,
+}));
+
+//#endregion
+
+describe("Ability - Guts", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .ability(AbilityId.GUTS)
+      .battleType("single")
+      .disableCrits()
+      .enemySpecies(SpeciesId.MAGIKARP)
+      .enemyAbility(AbilityId.BALL_FETCH)
+      .enemyMoveset(MoveId.SPLASH)
+      .startingLevel(9)
+      .enemyLevel(10);
+  });
+
+  it("should not apply an attack boost with no status effect", async () => {
+    const { field, classicMode } = game;
+
+    await classicMode.startBattle([SpeciesId.FEEBAS]);
+    const player = field.getPlayerPokemon();
+    game.move.use(MoveId.SPLASH);
+    await game.toEndOfTurn();
+    const playerAtk = player.getStat(Stat.ATK);
+
+    expect(player.hasNonVolatileStatusEffect()).toBe(false);
+    expect(player).toHaveEffectiveStat(Stat.ATK, playerAtk);
+  });
+
+  it("should not apply a defense boost with confusion status effect", async () => {
+    const { field, classicMode, move } = game;
+
+    await classicMode.startBattle([SpeciesId.FEEBAS]);
+    const player = field.getPlayerPokemon();
+    game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+    move.use(MoveId.SPLASH);
+    await move.selectEnemyMove(MoveId.CONFUSE_RAY);
+    await move.forceHit();
+    await game.toEndOfTurn();
+    const playerAtk = player.getStat(Stat.ATK);
+
+    expect(player.hasNonVolatileStatusEffect()).toBe(false);
+    expect(player).toHaveEffectiveStat(Stat.ATK, playerAtk);
+  });
+
+  it.each(nonVolatileStatusEffects)(
+    "should apply a 1.5x attack boost with $statusEffectName status effect",
+    async ({ statusEffectId }) => {
+      const { override, classicMode, field } = game;
+      override.statusEffect(statusEffectId);
+
+      await classicMode.startBattle([SpeciesId.FEEBAS]);
+      const player = field.getPlayerPokemon();
+      game.move.use(MoveId.SPLASH);
+      await game.toEndOfTurn();
+      const playerAtk = player.getStat(Stat.ATK);
+
+      expect(player.hasNonVolatileStatusEffect()).toBe(true);
+      expect(player).toHaveEffectiveStat(Stat.ATK, Math.floor(playerAtk * 1.5));
+    },
+  );
+
+  it.each([
+    { id: AbilityId.HUSTLE, name: "Hustle", multiplier: 1.5 },
+    { id: AbilityId.PURE_POWER, name: "Pure Power", multiplier: 2.0 },
+    { id: AbilityId.HUGE_POWER, name: "Huge Power", multiplier: 2.0 },
+  ])("should stack with $name ability", async ({ id, multiplier }) => {
+    const { override, field, classicMode } = game;
+    override.statusEffect(StatusEffect.PARALYSIS).passiveAbility(id);
+
+    await classicMode.startBattle([SpeciesId.FEEBAS]);
+    const player = field.getPlayerPokemon();
+    game.move.use(MoveId.SPLASH);
+    await game.toEndOfTurn();
+    const playerAtk = player.getStat(Stat.ATK);
+
+    expect(player.hasNonVolatileStatusEffect()).toBe(true);
+    expect(player).toHaveEffectiveStat(Stat.ATK, Math.floor(playerAtk * 1.5 * multiplier));
+  });
+});
