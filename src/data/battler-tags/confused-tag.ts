6c7289e6efc9523a762511f379bbf9a48352e83a
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-22 11:50:30 -0700

    chore: ConfusedTag docs cleanup

--- src/data/battler-tags/confused-tag.ts
+++ src/data/battler-tags/confused-tag.ts
@@ -82,7 +82,7 @@ export class ConfusedTag extends BattlerTag {
 
   /**
    * Helper function for checking if Confusion activates and retrieving self-inflicted damage from confusion
-   * @param pokemon the confused Pokemon
+   * @param pokemon - the confused Pokemon
    * @returns the amount of damage inflicted
    */
   public getDamage(pokemon: Pokemon): number {
