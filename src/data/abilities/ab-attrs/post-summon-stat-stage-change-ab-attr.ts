4034fccfd1a5edd375e378571eb8a1c598a603cc
NightKev <34855794+DayKev@users.noreply.github.com>
2025-05-26 09:21:56 -0700

    Fix Intimidate

--- src/data/abilities/ab-attrs/post-summon-stat-stage-change-ab-attr.ts
+++ src/data/abilities/ab-attrs/post-summon-stat-stage-change-ab-attr.ts
@@ -40,9 +40,8 @@ export class PostSummonStatStageChangeAbAttr extends PostSummonAbAttr {
     for (const opponent of pokemon.getOpponents()) {
       const cancelled = new BooleanHolder(false);
       if (this.intimidate) {
-        // TODO: this seems incorrect, doesn't this mean if either opponent has a sub up both of them won't be intimidated?
         if (opponent.hasTag(BattlerTagType.SUBSTITUTE)) {
-          return false;
+          continue;
         }
 
         applyAbAttrs<IntimidateImmunityAbAttr>(AbAttrFlag.INTIMIDATE_IMMUNITY, opponent, simulated, cancelled);
