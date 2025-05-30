610227f188e82c24766fe66068c4032230006bd5
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-30 09:47:34 -0700

    Update src/phases/turn-end-phase.ts
    Co-authored-by: Moka <54149968+MokaStitcher@users.noreply.github.com>

--- src/phases/turn-end-phase.ts
+++ src/phases/turn-end-phase.ts
@@ -40,7 +40,8 @@ export class TurnEndPhase extends FieldPhase {
           );
         }
         applyAbAttrs<PostTurnAbAttr>(AbAttrFlag.POST_TURN, pokemon, false);
-        applyAbAttrs<PostTurnAbAttr>(AbAttrFlag.BAD_DREAMS, pokemon, false); // TODO: THIS IS A WORKAROUND SO BAD DREAMS DOESN'T HURT POKEMON WAKING UP IN THE SAME TURN. Has to be fixed with #1211
+       // TODO: Temporary workaround so that bad dreams doesn't hurt Pokemon waking up in the same turn. Has to be fixed with #1211
+        applyAbAttrs<PostTurnAbAttr>(AbAttrFlag.BAD_DREAMS, pokemon, false);
       }
 
       globalScene.applyModifiers(TurnStatusEffectModifier, pokemon.isPlayer(), pokemon);
