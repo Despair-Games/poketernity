b142d4b92ea79492cfe67dd097adf3bb04a8710c
flx-sta <50131232+flx-sta@users.noreply.github.com>
2025-05-30 16:01:31 -0700

    update ReceivedTypeDamageMultiplierAbAttr
    - add docs

--- src/data/abilities/ab-attrs/received-type-damage-multiplier-ab-attr.ts
+++ src/data/abilities/ab-attrs/received-type-damage-multiplier-ab-attr.ts
@@ -1,6 +1,20 @@
 import { ReceivedMoveDamageMultiplierAbAttr } from "#abilities/received-move-damage-multiplier-ab-attr";
 import type { ElementalType } from "#enums/elemental-type";
 
+/**
+ * Adds a damage multiplier for a specific {@linkcode ElementalType}
+ * when the ability holder is hit by a move of that type.
+ *
+ * ```
+ * +-----------+------------------+
+ * |  Ability  |    Multiplier    |
+ * +-----------+------------------+
+ * | Dry Skin  |   1.25 (+25%)    |
+ * +-----------+------------------+
+ * ```
+ *
+ * @extends ReceivedMoveDamageMultiplierAbAttr
+ */
 export class ReceivedTypeDamageMultiplierAbAttr extends ReceivedMoveDamageMultiplierAbAttr {
   constructor(moveType: ElementalType, damageMultiplier: number) {
     super((_target, user, move) => user.getMoveType(move) === moveType, damageMultiplier);
