29745482fe179456042003607f7232b6b20ce403
NightKev <34855794+DayKev@users.noreply.github.com>
2025-05-30 22:29:01 -0700

    Convert `DamageNumbersMode` to `const` object

--- src/enums/damage-numbers-mode.ts
+++ src/enums/damage-numbers-mode.ts
@@ -1,5 +1,7 @@
-export enum DamageNumbersMode {
-  OFF,
-  SIMPLE,
-  FANCY,
-}
+export const DamageNumbersMode = {
+  OFF: 0,
+  SIMPLE: 1,
+  FANCY: 2,
+} as const;
+
+export type DamageNumbersMode = (typeof DamageNumbersMode)[keyof typeof DamageNumbersMode];
