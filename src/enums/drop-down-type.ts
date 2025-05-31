43de42267231189c9c9a3415714c6c7c88523b9e
NightKev <34855794+DayKev@users.noreply.github.com>
2025-05-30 22:34:24 -0700

    Convert `DropDownType` to `const` object

--- src/enums/drop-down-type.ts
+++ src/enums/drop-down-type.ts
@@ -1,6 +1,8 @@
-export enum DropDownType {
-  SINGLE,
-  MULTI,
-  HYBRID,
-  RADIAL,
-}
+export const DropDownType = {
+  SINGLE: 1,
+  MULTI: 2,
+  HYBRID: 3,
+  RADIAL: 4,
+} as const;
+
+export type DropDownType = (typeof DropDownType)[keyof typeof DropDownType];
