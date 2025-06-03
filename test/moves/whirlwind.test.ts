5fc09744ef23f738be98146dc7ff660c69d56586
NightKev <34855794+DayKev@users.noreply.github.com>
2025-06-02 19:48:33 -0700

    Use `.toHaveBattlerTagType` matcher for test
    Co-authored-by: flx-sta <50131232+flx-sta@users.noreply.github.com>

--- test/moves/whirlwind.test.ts
+++ test/moves/whirlwind.test.ts
@@ -42,7 +42,7 @@ describe("Moves - Whirlwind", () => {
 
     await game.toEndOfTurn();
 
-    expect(staraptor.hasTag(BattlerTagType.MID_AIR)).toBeTruthy();
+    expect(staraptor).toHaveBattlerTagType(BattlerTagType.MID_AIR);
     expect(game.field.getEnemyPokemon()).toHaveMoveResult(MoveResult.MISS);
   });
 
