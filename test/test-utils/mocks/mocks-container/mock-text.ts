97664a9298419ddf85a8aeed4e434ec464cecb74
Moka <millennium.stitcher@gmail.com>
2025-07-18 16:54:42 +0200

    fix test mocks and compilation error caused to rebase

--- test/test-utils/mocks/mocks-container/mock-text.ts
+++ test/test-utils/mocks/mocks-container/mock-text.ts
@@ -11,6 +11,7 @@ export class MockText implements MockGameObject {
   public style;
   public text = "";
   public name: string;
+  public maxLines: number;
   public color?: string;
   public active = true;
 
@@ -190,6 +191,11 @@ export class MockText implements MockGameObject {
     // Sets the text this Game Object will display.
     // return this.phaserText.setText\(text);
     this.text = text;
+    return this;
+  }
+
+  setMaxLines(maxLines: number): this {
+    this.maxLines = maxLines;
     return this;
   }
 
