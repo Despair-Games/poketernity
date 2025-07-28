5a4c7037a1bb57accb8bda81a2f70881e51aa741
NightKev <34855794+DayKev@users.noreply.github.com>
2025-07-27 21:07:07 -0700

    Convert `HitCheckResult` to `const` object

--- src/enums/hit-check-result.ts
+++ src/enums/hit-check-result.ts
@@ -1,17 +1,21 @@
+import type { EnumValues } from "#types/utility-types";
+
 /** Descriptor for the outcome of a move being used against a target */
-export enum HitCheckResult {
+export const HitCheckResult = {
   /** Hit checks haven't been evaluated yet in this pass */
-  PENDING,
+  PENDING: 1,
   /** The move hits the target successfully */
-  HIT,
+  HIT: 2,
   /** The move has no effect on the target */
-  NO_EFFECT,
+  NO_EFFECT: 3,
   /** The move has no effect on the target, but doesn't proc the default "no effect" message. */
-  NO_EFFECT_NO_MESSAGE,
+  NO_EFFECT_NO_MESSAGE: 4,
   /** The target protected itself against the move */
-  PROTECTED,
+  PROTECTED: 5,
   /** The move missed the target */
-  MISS,
+  MISS: 6,
   /** The move failed unexpectedly */
-  ERROR,
-}
+  ERROR: 7,
+} as const;
+
+export type HitCheckResult = EnumValues<typeof HitCheckResult>;
