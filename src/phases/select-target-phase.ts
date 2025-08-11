355152091bdf07e88e9ca3535f35f765b5107a43
Moka <millennium.stitcher@gmail.com>
2025-08-11 17:00:31 +0200

    add missing calls to getPokemonMoveName

--- src/phases/select-target-phase.ts
+++ src/phases/select-target-phase.ts
@@ -5,6 +5,7 @@ import { MoveId } from "#enums/move-id";
 import { UiMode } from "#enums/ui-mode";
 import { PokemonPhase } from "#phases/base/pokemon-phase";
 import type { TargetSelectUiHandler } from "#ui/target-select-ui-handler";
+import { getPokemonMoveName } from "#utils/pokemon-utils";
 import i18next from "i18next";
 
 export class SelectTargetPhase extends PokemonPhase {
@@ -33,7 +34,7 @@ export class SelectTargetPhase extends PokemonPhase {
           .getRestrictingTag(moveId, user, firstTarget)
           ?.getSelectionDeniedText(user, moveObject.id);
 
-        const moveName = user.getPokemonMove(moveObject.id)?.name ?? allMoves.get(moveId).name;
+        const moveName = getPokemonMoveName(user, moveId);
         globalScene.phaseManager.createAndUnshiftPhase(
           "MessagePhase",
           errorMessage ?? i18next.t("battle:moveCannotBeSelected", { moveName }),
