c25b2ba24703bee0dabe7a93a1328a9213b6b270
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-08-14 03:15:19 -0700

    Add scoring for `BypassSleepAttr`

--- src/data/moves/move-attrs/bypass-sleep-attr.ts
+++ src/data/moves/move-attrs/bypass-sleep-attr.ts
@@ -1,5 +1,7 @@
+import { BAD_MOVE_PENALTY } from "#constants/ai-constants";
 import { BattlerTagType } from "#enums/battler-tag-type";
 import { StatusEffect } from "#enums/status-effect";
+import type { EnemyPokemon } from "#field/enemy-pokemon";
 import type { Pokemon } from "#field/pokemon";
 import type { Move } from "#moves/move";
 import { MoveAttr } from "#moves/move-attr";
@@ -10,7 +12,7 @@ import { MoveAttr } from "#moves/move-attr";
  * and {@link https://bulbapedia.bulbagarden.net/wiki/Sleep_Talk_(move) | Sleep Talk}.
  */
 export class BypassSleepAttr extends MoveAttr {
-  override apply(user: Pokemon, _target: Pokemon | null, move: Move): boolean {
+  public override apply(user: Pokemon, _target: Pokemon | null, move: Move): boolean {
     if (user.hasStatusEffect(StatusEffect.SLEEP)) {
       user.addTag(BattlerTagType.BYPASS_SLEEP, 1, move.id, user.id);
       return true;
@@ -19,8 +21,16 @@ export class BypassSleepAttr extends MoveAttr {
     return false;
   }
 
-  /** Returns arbitrarily high score when Pokemon is asleep, otherwise shouldn't be used */
-  override getUserBenefitScore(user: Pokemon, _target: Pokemon, _move: Move): number {
-    return user.hasStatusEffect(StatusEffect.SLEEP) ? 200 : -10;
+  /**
+   * @returns
+   * - (+6) if the user is asleep, minus (2) for each turn the user has already slept.
+   * - A {@linkcode BAD_MOVE_PENALTY} if the user is not asleep
+   * @todo Should the penalty be enforced by Snore/Sleep Talk's conditions instead?
+   */
+  public override getEffectScore(user: EnemyPokemon, _target: Pokemon, _move: Move): number {
+    if (user.hasStatusEffect(StatusEffect.SLEEP)) {
+      return 6 - 2 * user.turnsAsleep;
+    }
+    return BAD_MOVE_PENALTY;
   }
 }
