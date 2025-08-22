f863b7daea3b48c3ab85434da879c23a6b71f80f
NightKev <34855794+DayKev@users.noreply.github.com>
2025-08-21 21:24:59 -0700

    Add missing `as const` to `MoveEffectTrigger`

--- src/enums/move-effect-trigger.ts
+++ src/enums/move-effect-trigger.ts
@@ -5,6 +5,6 @@ export const MoveEffectTrigger = {
   POST_APPLY: 2,
   /** Triggers one time after all target effects have applied */
   POST_TARGET: 3,
-};
+} as const;
 
 export type MoveEffectTrigger = EnumValues<typeof MoveEffectTrigger>;
