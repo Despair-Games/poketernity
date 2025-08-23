594e221b7bf6e3373004dbabe5ce500043cf641d
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-08-23 02:23:30 -0700

    Fix broken docs link

--- src/data/moves/move-attrs/move-attr.ts
+++ src/data/moves/move-attrs/move-attr.ts
@@ -114,7 +114,7 @@ export abstract class MoveAttr {
    * current game state when targeting an ally.
    * @param user - The {@linkcode EnemyPokemon} evaluating the move
    * @param target - The {@linkcode Pokemon} the move is evaluated against. This can be assumed to be
-   * the {@linkcode user}'s ally.
+   * the {@linkcode _user | user}'s ally.
    * @param move - The {@linkcode Move} being evaluated. This can be assumed to be a Status move since
    * all Attack moves are automatically given an {@linkcode ALLY_TARGET_PENALTY}.
    * @returns `null` by default. `null` scores do not contribute to Effect Score, but can warrant an
