05a5f2936cde04950ef785ed0409c920a6a933a9
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-08-28 03:03:52 -0700

    Add scoring for `PostVictoryStatStageChangeAttr` and `InvertStatsAttr`

--- src/data/moves/move-attrs/invert-stats-attr.ts
+++ src/data/moves/move-attrs/invert-stats-attr.ts
@@ -1,6 +1,8 @@
 import { globalScene } from "#app/global-scene";
 import { getPokemonNameWithAffix } from "#app/messages";
+import { MINOR_EFFECT_SCORE_BONUS, MINOR_EFFECT_SCORE_PENALTY, SOFT_EFFECT_SCORE_LIMIT } from "#constants/ai-constants";
 import { BATTLE_STATS } from "#enums/stat";
+import type { EnemyPokemon } from "#field/enemy-pokemon";
 import type { Pokemon } from "#field/pokemon";
 import type { Move } from "#moves/move";
 import { MoveEffectAttr } from "#moves/move-effect-attr";
@@ -11,7 +13,7 @@ import i18next from "i18next";
  * Used for {@link https://bulbapedia.bulbagarden.net/wiki/Topsy-Turvy_(move) | Topsy-Turvy}.
  */
 export class InvertStatsAttr extends MoveEffectAttr {
-  override applyEffect(user: Pokemon, target: Pokemon, _move: Move): boolean {
+  public override applyEffect(user: Pokemon, target: Pokemon, _move: Move): boolean {
     for (const s of BATTLE_STATS) {
       target.setStatStage(s, -target.getStatStage(s));
     }
@@ -25,5 +27,30 @@ export class InvertStatsAttr extends MoveEffectAttr {
     );
 
     return true;
+  }
+
+  /**
+   * @returns an Effect Score modifier equal to the effective stat stages gained from this effect
+   * multiplied by {@linkcode MINOR_EFFECT_SCORE_PENALTY}. This modifier cannot exceed the {@linkcode SOFT_EFFECT_SCORE_LIMIT}.
+   */
+  public override getEffectScore(_user: EnemyPokemon, target: Pokemon, _move: Move): number {
+    return Math.min(this.getProjectedStatChange(target) * MINOR_EFFECT_SCORE_PENALTY, SOFT_EFFECT_SCORE_LIMIT);
+  }
+
+  /**
+   * @returns an Effect Score modifier equal to the effective stat stages gained from this effect
+   * multiplied by {@linkcode MINOR_EFFECT_SCORE_BONUS}. This modifier cannot exceed the {@linkcode SOFT_EFFECT_SCORE_LIMIT}.
+   */
+  public override getAllyTargetScore(_user: EnemyPokemon, target: EnemyPokemon, _move: Move): number {
+    return Math.min(this.getProjectedStatChange(target) * MINOR_EFFECT_SCORE_BONUS, SOFT_EFFECT_SCORE_LIMIT);
+  }
+
+  /**
+   * @param target - The {@linkcode Pokemon} on which this effect is evaluated
+   * @returns The effective change (in number of stat stages) this attribute's effect is expected
+   * to apply to the given target
+   */
+  private getProjectedStatChange(target: Pokemon) {
+    return BATTLE_STATS.reduce((total, stat) => total + target.getStatStage(stat) * -2, 0);
   }
 }
--- src/data/moves/move-attrs/post-victory-stat-stage-change-attr.ts
+++ src/data/moves/move-attrs/post-victory-stat-stage-change-attr.ts
@@ -1,4 +1,7 @@
+import { BAD_MOVE_PENALTY, KO_ATTACK_SCORE } from "#constants/ai-constants";
+import { AbilityId } from "#enums/ability-id";
 import type { BattleStat } from "#enums/stat";
+import type { EnemyPokemon } from "#field/enemy-pokemon";
 import type { Pokemon } from "#field/pokemon";
 import type { Move } from "#moves/move";
 import { MoveAttr } from "#moves/move-attr";
@@ -30,11 +33,27 @@ export class PostVictoryStatStageChangeAttr extends MoveAttr {
     this.showMessage = showMessage;
   }
 
-  applyPostVictory(user: Pokemon, target: Pokemon, move: Move): void {
+  /** @todo Override {@linkcode apply} instead */
+  public applyPostVictory(user: Pokemon, target: Pokemon, move: Move): void {
     if (this.condition && !this.condition(user, target, move)) {
       return;
     }
     const statChangeAttr = new StatStageChangeAttr(this.stats, this.stages, this.showMessage);
     statChangeAttr.applyEffect(user, target, move);
+  }
+
+  /**
+   * @returns (+3) if the user is expected to KO the target with the given move. Combined with Attack Score, this
+   * means a KO will yield a (+7) score, meaning the AI will prioritize moves with this effect over moves with
+   * high priority. Under the same conditions, if the user has Contrary, this grants a {@linkcode BAD_MOVE_PENALTY}.
+   */
+  public override getEffectScore(user: EnemyPokemon, target: Pokemon, move: Move): number {
+    if (
+      user.getExpectedAttackScore(target, move) >= KO_ATTACK_SCORE
+      && this.stats.some((stat) => user.getStatStage(stat) < 6)
+    ) {
+      return user.hasAbility(AbilityId.CONTRARY) ? BAD_MOVE_PENALTY : 3;
+    }
+    return 0;
   }
 }
