06beb4b96684dde668c1110dba48fff0f9b3ecb4
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-09-03 23:59:51 -0700

    Add disclaimer to `playTween` docs

--- src/utils/anim-utils.ts
+++ src/utils/anim-utils.ts
@@ -73,6 +73,12 @@ export async function loadEncounterAnimAssets(startLoad?: boolean): Promise<void
  * Plays a Tween animation, resolving once the animation completes.
  * @param config - The config for a single Tween
  * @param scene - The {@linkcode SceneBase} on which the Tween plays (Default {@linkcode globalScene})
+ *
+ * @privateRemarks
+ * The `config` input should not include an `onComplete` field as that callback is
+ * used to resolve the Promise containing the Tween animation. However, `config`'s type
+ * cannot be changed to something like `Omit<TweenBuilderConfig, "onComplete">` due to
+ * how Phaser interprets `TweenBuilderConfig` inputs.
  */
 export async function playTween(config: TweenBuilderConfig, scene: SceneBase = globalScene): Promise<void> {
   await new Promise((resolve) =>
