84ed2a04888772ed3bb02600a2b478eadf4e47a3
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-09-13 00:58:29 -0700

    Add Effect Scores for unscored charge attributes

--- src/data/moves/move-attrs/semi-invulnerable-attr.ts
+++ src/data/moves/move-attrs/semi-invulnerable-attr.ts
@@ -1,4 +1,7 @@
-import type { BattlerTagType } from "#enums/battler-tag-type";
+import { MINOR_EFFECT_SCORE_BONUS } from "#constants/ai-constants";
+import { AbAttrFlag } from "#enums/ab-attr-flag";
+import { BattlerTagType } from "#enums/battler-tag-type";
+import type { EnemyPokemon } from "#field/enemy-pokemon";
 import type { Pokemon } from "#field/pokemon";
 import type { Move } from "#moves/move";
 import { MoveEffectAttr } from "#moves/move-effect-attr";
@@ -18,7 +21,28 @@ export class SemiInvulnerableAttr extends MoveEffectAttr {
     this.tagType = tagType;
   }
 
-  override applyEffect(user: Pokemon, _target: Pokemon, move: Move): boolean {
+  public override applyEffect(user: Pokemon, _target: Pokemon, move: Move): boolean {
     return user.addTag(this.tagType, 1, move.id, user.id);
+  }
+
+  /**
+   * @returns A {@linkcode MINOR_EFFECT_SCORE_BONUS} if the following conditions apply:
+   * - The user is faster than all of its active opponents
+   * - All opponents cannot bypass the effects of semi-invulnerability (e.g. via No Guard)
+   *
+   * This effectively halves the penalty applied to charge moves under favorable conditions.
+   */
+  public override getEffectScore(user: EnemyPokemon, _target: Pokemon, _move: Move): number {
+    const opponents = user.getOpponents();
+    /**
+     * @todo
+     * - This will detect opponents' No Guard even when it hasn't been revealed yet
+     * - Opponents' Lock On is counted even when the opponent "locked on" to another Pokemon
+     */
+    const canBypass =
+      [user, ...opponents].some((p) => p.hasAbilityWithAttr(AbAttrFlag.ALWAYS_HIT))
+      || opponents.some((opp) => opp.hasTag(BattlerTagType.IGNORE_ACCURACY));
+
+    return !canBypass && opponents.every((opp) => user.outspeeds(opp, true)) ? MINOR_EFFECT_SCORE_BONUS : 0;
   }
 }
--- src/data/moves/move-attrs/sky-drop-attr.ts
+++ src/data/moves/move-attrs/sky-drop-attr.ts
@@ -1,5 +1,7 @@
+import { MAJOR_EFFECT_SCORE_BONUS } from "#constants/ai-constants";
 import { MOVE_LOCK_TAG_TYPES, SEMI_INVULNERABLE_BATTLER_TAG_TYPES } from "#constants/battler-tag-constants";
 import { BattlerTagType } from "#enums/battler-tag-type";
+import type { EnemyPokemon } from "#field/enemy-pokemon";
 import type { Pokemon } from "#field/pokemon";
 import { failOnGravityCondition } from "#moves/fail-on-gravity-condition";
 import type { Move } from "#moves/move";
@@ -43,5 +45,13 @@ export class SkyDropAttr extends MoveEffectAttr {
       && !target.hasTag(...SEMI_INVULNERABLE_BATTLER_TAG_TYPES, BattlerTagType.SUBSTITUTE)
       && target.getAlly()?.getTag(BattlerTagType.COMMANDED)?.getSourcePokemon()?.id !== target.id
       && (!target.hasTag(BattlerTagType.SKY_DROP) || target.getTag(BattlerTagType.SKY_DROP)?.sourceId === user.id);
+  }
+
+  /**
+   * @returns A {@linkcode MAJOR_EFFECT_SCORE_BONUS} if the user is faster than all of its opponents.
+   * This effectively negates the penalty applied to charge moves under favorable conditions.
+   */
+  public override getEffectScore(user: EnemyPokemon, _target: Pokemon, _move: Move): number {
+    return user.getOpponents().every((opp) => user.outspeeds(opp, true)) ? MAJOR_EFFECT_SCORE_BONUS : 0;
   }
 }
