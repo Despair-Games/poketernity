d0dc9327097c439724754f7fc16b07c00ae21a8d
NightKev <34855794+DayKev@users.noreply.github.com>
2025-09-14 18:32:16 -0700

    Also prevent Judgement, Multi-Attack, Terrain Pulse from type change

--- src/data/abilities/ab-attrs/move-type-change-ab-attr.ts
+++ src/data/abilities/ab-attrs/move-type-change-ab-attr.ts
@@ -2,8 +2,10 @@ import { PreAttackAbAttr } from "#abilities/pre-attack-ab-attr";
 import { AbAttrFlag } from "#enums/ab-attr-flag";
 import type { ElementalType } from "#enums/elemental-type";
 import type { Pokemon } from "#field/pokemon";
+import { FormChangeItemTypeAttr } from "#moves/form-change-item-type-attr";
 import { MatchUserTypeAttr } from "#moves/match-user-type-attr";
 import type { Move } from "#moves/move";
+import { TerrainPulseTypeAttr } from "#moves/terrain-pulse-type-attr";
 import type { PokemonAttackCondition } from "#types/move-types";
 import type { NumberHolder } from "#utils/common-utils";
 
@@ -30,8 +32,13 @@ export class MoveTypeChangeAbAttr extends PreAttackAbAttr {
     moveType?: NumberHolder,
     power?: NumberHolder,
   ): boolean {
-    // Revelation Dance overrides ability-based move type changes
-    if (move.hasAttr(MatchUserTypeAttr) || !this.condition(pokemon, defender, move)) {
+    // Revelation Dance, Judgement and Multi-Attack, and Terrain Pulse ignore ability-based move type changes
+    if (
+      move.hasAttr(MatchUserTypeAttr)
+      || move.hasAttr(FormChangeItemTypeAttr)
+      || move.hasAttr(TerrainPulseTypeAttr)
+      || !this.condition(pokemon, defender, move)
+    ) {
       return false;
     }
     if (moveType != null) {
