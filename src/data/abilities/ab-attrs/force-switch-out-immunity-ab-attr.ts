9a5e1cc48c978eb40c52a30a7b42239a34a8fcde
innerthunder <168692175+innerthunder@users.noreply.github.com>
2025-10-09 22:03:52 -0700

    Add docs + TODO for `ForceSwitchOutImmunityAbAttr`

--- src/data/abilities/ab-attrs/force-switch-out-immunity-ab-attr.ts
+++ src/data/abilities/ab-attrs/force-switch-out-immunity-ab-attr.ts
@@ -3,6 +3,13 @@ import { AbAttrFlag } from "#enums/ab-attr-flag";
 import type { Pokemon } from "#field/pokemon";
 import type { ValueHolder } from "#utils/common-utils";
 
+/**
+ * Ability attribute to grant immunity against effects that would force the
+ * source Pokemon to switch out or flee, e.g. Roar, Whirlwind, etc.
+ * @todo The activation message when this attribute applies should be implemented
+ * internally. It being implemented externally (as it is now) prevents the ability flyout
+ * from functioning.
+ */
 export class ForceSwitchOutImmunityAbAttr extends AbAttr {
   constructor() {
     super();
