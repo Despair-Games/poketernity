c42362537358b7d34fa22b88e09eadcae14861f1
NightKev <34855794+DayKev@users.noreply.github.com>
2025-10-12 12:19:10 -0700

    Add linebreak to doc

--- src/data/moves/move-attrs/pursuit-attr.ts
+++ src/data/moves/move-attrs/pursuit-attr.ts
@@ -7,7 +7,7 @@ import { MoveAttr } from "#moves/move-attr";
 
 /**
  * Attribute for moves that allow the user to attack an opposing Pokemon before it
- * switches out, i.e. {@link https://bulbapedia.bulbagarden.net/wiki/Pursuit_(move) | Pursuit}.
+ * switches out, i.e. {@link https://bulbapedia.bulbagarden.net/wiki/Pursuit_(move) | Pursuit}. \
  * When attacking a retreating target, moves with this attribute double in power.
  * @see {@linkcode TurnCommandManager.tryPursueTarget}
  * @see {@linkcode PursuingTag}
