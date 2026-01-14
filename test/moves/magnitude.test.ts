8bf06064dac0625ec2ba96159e9996514385bd4f
NightKev <34855794+DayKev@users.noreply.github.com>
2026-01-13 22:48:19 -0800

    fix(move): Magnitude power values and thresholds set to correct values

---
+++ test/moves/magnitude.test.ts
@@ -0,0 +1,53 @@
+import { allMoves } from "#data/data-lists";
+import { AbilityId } from "#enums/ability-id";
+import { MoveId } from "#enums/move-id";
+import { SpeciesId } from "#enums/species-id";
+import { MagnitudePowerAttr } from "#moves/magnitude-power-attr";
+import { GameManager } from "#test/test-utils/game-manager";
+import { ValueHolder } from "#utils/common-utils";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+describe("Move - Magnitude", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .ability(AbilityId.BALL_FETCH)
+      .battleType("single")
+      .disableCrits()
+      .enemySpecies(SpeciesId.MAGIKARP)
+      .enemyAbility(AbilityId.BALL_FETCH)
+      .enemyMoveset(MoveId.SPLASH)
+      .startingLevel(100)
+      .enemyLevel(100);
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  it("should have the correct rng thresholds and power values", async () => {
+    await game.classicMode.runToSummon(SpeciesId.FEEBAS);
+
+    const magnitudeAttr = allMoves.get(MoveId.MAGNITUDE).getAttrs(MagnitudePowerAttr)[0];
+    const movePower = new ValueHolder(0);
+    const results = { [0]: 0, [10]: 0, [30]: 0, [50]: 0, [70]: 0, [90]: 0, [110]: 0, [150]: 0 };
+
+    await game.rng.equalSample(100, () => {
+      movePower.value = 0;
+      magnitudeAttr.apply(null!, null!, null!, movePower);
+      results[movePower.value]++;
+    });
+
+    expect(results).toMatchObject({ [0]: 0, [10]: 5, [30]: 10, [50]: 20, [70]: 30, [90]: 20, [110]: 10, [150]: 5 });
+  });
+});
