6a8a485db26aa661f0dd9729bfaa09f8dc8a0f89
NightKev <34855794+DayKev@users.noreply.github.com>
2026-01-18 22:29:10 -0800

    fix(ability): Color Change will now activate in the correct situations

--- src/data/abilities/ab-attrs/post-defend-type-change-ab-attr.ts
+++ src/data/abilities/ab-attrs/post-defend-type-change-ab-attr.ts
@@ -1,32 +1,54 @@
 import { PostDefendAbAttr } from "#abilities/post-defend-ab-attr";
 import { getPokemonNameWithAffix } from "#app/messages";
 import { ElementalType } from "#enums/elemental-type";
+import { TypelessAttr } from "#moves/typeless-attr";
 import type { PostDefendAbAttrParams } from "#types/ab-attr-param-types";
 import { enumValueToKey } from "#utils/common-utils";
 import i18next from "i18next";
 
 /** @see {@link https://bulbapedia.bulbagarden.net/wiki/Color_Change_(Ability)} */
 export class PostDefendTypeChangeAbAttr extends PostDefendAbAttr {
-  public override apply({ pokemon, simulated, attacker, move }: PostDefendAbAttrParams): void {
+  private type: ElementalType;
+
+  public override canApply({ pokemon, attacker, move }: Parameters<this["apply"]>[0]): boolean {
+    if (pokemon.isTerastallized) {
+      return false;
+    }
+
+    if (!move.isAttackMove(attacker, pokemon)) {
+      return false;
+    }
+
+    if (move.hasAttr(TypelessAttr)) {
+      return false;
+    }
+
+    if (attacker.turnData.hitsLeft > 1) {
+      return false;
+    }
+
+    this.type = attacker.getMoveType(move);
+
+    if (pokemon.isOfType(this.type, true, true)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public override apply({ pokemon, simulated }: PostDefendAbAttrParams): void {
     if (simulated) {
       return;
     }
 
-    pokemon.setTemporaryTypes(attacker.getMoveType(move));
-  }
-
-  public override canApply({ pokemon, attacker, move }: Parameters<this["apply"]>[0]): boolean {
-    const moveType = attacker.getMoveType(move);
-    const pokemonTypes = pokemon.getTypes(true, true);
-
-    return move.isAttackMove(attacker, pokemon) && !pokemon.isTerastallized && !pokemonTypes.includes(moveType);
+    pokemon.setTemporaryTypes(this.type);
   }
 
   public override getTriggerMessage({ pokemon }: Parameters<this["apply"]>[0], abilityName: string): string {
     return i18next.t("abilityTriggers:postDefendTypeChange", {
       pokemonNameWithAffix: getPokemonNameWithAffix(pokemon),
       abilityName,
-      typeName: i18next.t(`pokemonInfo:Type.${enumValueToKey(ElementalType, pokemon.getTypes(true)[0])}`),
+      typeName: i18next.t(`pokemonInfo:Type.${enumValueToKey(ElementalType, this.type)}`),
     });
   }
 }
